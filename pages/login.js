c20b088ce11b8a1439b5c9aa40e0c4716ab28ac1
liumin1128 <970568830@qq.com>
2017-11-11 18:55:12 +0800

    更新登录逻辑

---
+++ pages/login.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import Router from 'next/router';
+import { setStorage } from '../utils';
+import { reduxPage } from '../store';
+
+class Login extends Component {
+  // static async getInitialProps({ query }) {
+  //   return query;
+  // }
+  componentDidMount() {
+    const { token } = this.props.url.query;
+    setStorage('token', token);
+    this.props.dispatch({
+      type: 'user/getUserInfo',
+    });
+    Router.push('/');
+  }
+  render() {
+    const { token } = this.props;
+    return (<div>
+      <p>{token}</p>
+      <h1>登录成功</h1>
+    </div>);
+  }
+}
+
+export default reduxPage(Login);
+
