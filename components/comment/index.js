c15b72a0404565a6de0dfb9cd48bfe1d94e87156
liumin1128 <970568830@qq.com>
2017-11-21 21:33:48 +0800

    评论支持

--- components/comment/index.js
+++ components/comment/index.js
@@ -1,9 +1,11 @@
 import React, { PureComponent } from 'react';
 import { connect } from '../../utils';
 import Creater from '../user/avatar-nickname-meta';
+import Loading from '../loading';
 
 @connect(({ comment }) => ({
   list: comment.list,
+  isEnd: comment.isEnd,
 }))
 export default class extends PureComponent {
   constructor(props) {
@@ -12,17 +14,21 @@ export default class extends PureComponent {
       const { id, dispatch } = this.props;
       dispatch({ type: 'comment/create', payload: { content: '2121', id } });
     };
+    this.more = () => {
+      const { id, dispatch } = this.props;
+      dispatch({ type: 'comment/more', payload: { id } });
+    };
   }
   componentWillMount() {
     const { id, dispatch } = this.props;
     dispatch({ type: 'comment/init', payload: { id } });
   }
   render() {
-    const { id, list = [] } = this.props;
+    const { id, list = [], isEnd } = this.props;
     return (<div>
       <div className="comments card">
         {
-          list.map(i => (<div>
+          list.map(i => (<div className="comment-item">
             <Creater
               avatarUrl={i.user.avatarUrl}
               nickname={i.user.nickname}
@@ -32,9 +38,18 @@ export default class extends PureComponent {
           </div>))
         }
       </div>
+      <Loading
+        isEnd={isEnd}
+        onEnter={this.more}
+        // onLeave={leave}
+      />
       <button onClick={this.comment}>77777</button>
       <style jsx>{`
         .comments {
+          padding: 0px;
+        }
+        .comment-item {
+          border-top: 1px rgba(0,0,0,0.05) solid;
           padding: 16px;
         }
       `}</style>
