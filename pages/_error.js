9c38f17372dbab2b4b8498951118f0b5f00c116d
liumin1128 <970568830@qq.com>
2017-11-28 12:48:08 +0800

    更新

---
+++ pages/_error.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default class Error extends React.Component {
+  static getInitialProps({ res, err }) {
+    const statusCode = res ? res.statusCode : err ? err.statusCode : null;
+    return { statusCode };
+  }
+
+  render() {
+    return (
+      <p>
+        {this.props.statusCode
+          ? `An error ${this.props.statusCode} occurred on server`
+          : 'An error occurred on client'}
+        <a href="/">回到首页</a>
+      </p>
+    );
+  }
+}
