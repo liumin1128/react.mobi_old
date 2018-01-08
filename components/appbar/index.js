48ac25c07a1dedbfb33b21047476848094a88888
liumin1128 <970568830@qq.com>
2018-01-08 22:06:16 +0800

    更新

--- components/appbar/index.js
+++ components/appbar/index.js
@@ -143,7 +143,7 @@ export default class extends PureComponent {
                   <Button onClick={() => { Router.push('/'); }} className={classes.nav} color="contrast">首页</Button>
                   <Button onClick={() => { Router.push('/article'); }} className={classes.nav} color="contrast">文章</Button>
                   <Button onClick={() => { Router.push('/say'); }} className={classes.nav} color="contrast">话题</Button>
-                  <Button onClick={() => { Router.push('/expore'); }} className={classes.nav} color="contrast">发现</Button>
+                  <Button onClick={() => { Router.push('/explore'); }} className={classes.nav} color="contrast">发现</Button>
                 </div>
                 <Input
                   id="adornment-password"
---
+++ pages/explore/index.js
@@ -0,0 +1,68 @@
+import React, { PureComponent } from 'react';
+import Grid from 'material-ui/Grid';
+import { withStyles } from 'material-ui/styles';
+import { withReduxSaga } from '../../store';
+import AppBar from '../../components/appbar';
+import withRoot from '../../components/material-ui/withRoot';
+import Explore from '../../container/explore';
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    maxWidth: 1110,
+    margin: 'auto',
+    marginTop: 32,
+  },
+  body: {
+    maxWidth: 760,
+    width: '100%',
+    margin: '0 auto',
+  },
+  container: {
+    boxSizing: 'border-box',
+    margin: 0,
+    // border: '1px red solid',
+    width: '100%',
+    '@media (max-width: 960px)': {
+      margin: 0,
+    },
+  },
+});
+
+@withReduxSaga
+@withRoot
+@withStyles(styles)
+export default class extends PureComponent {
+  static async getInitialProps({ query, store }) {
+    await store.dispatch({
+      type: 'say/init',
+      payload: query,
+    });
+    return query;
+  }
+  componentWillMount() {
+    const { dispatch } = this.props;
+    dispatch({
+      type: 'say/init',
+      // payload: query,
+    });
+  }
+  render() {
+    const { classes } = this.props;
+    return (
+      <div>
+        <AppBar />
+        <div className={classes.root}>
+          <Grid className={classes.container} container spacing={24}>
+            <Grid item xs={12} sm={12} md={4} lg={3}>
+              <div className={classes.body}>
+                <Explore />
+              </div>
+            </Grid>
+          </Grid>
+        </div>
+      </div>
+    );
+  }
+}
+
