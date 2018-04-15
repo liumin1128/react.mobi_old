d391c672d69066590363b9c732af4caaa6bd6381
liumin1128 <970568830@qq.com>
2018-04-15 19:18:59 +0800

    更新

--- view/meizitu/detail.js
+++ view/meizitu/detail.js
@@ -1,17 +1,28 @@
 import React, { PureComponent } from 'react';
 import { Query } from 'react-apollo';
+import { withStyles } from 'material-ui/styles';
 import { MEIZITU_DETAIL } from '../../graphql/meizitu';
 
+
+const styles = theme => ({
+  root: {
+    '& > img': {
+      width: '100%',
+    },
+  },
+});
+@withStyles(styles)
 export default class MeizituDetail extends PureComponent {
   render() {
     const _id = this.props.query.id;
+    const { classes } = this.props;
     return (<Query query={MEIZITU_DETAIL} variables={{ _id }}>
       {({ loading, error, data = {} }) => {
         const { detail = {} } = data;
         if (loading) return 'Loading...';
         if (error) return `Error! ${error.message}`;
         return (
-          <div>
+          <div className={classes.root}>
             {detail.title}
             {detail.pictures.map(i => <img src={i} alt="" />)}
           </div>
