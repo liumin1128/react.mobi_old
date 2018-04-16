a97a9f40d7f8372dc6a8b015198701dca1d71fd3
liumin1128 <970568830@qq.com>
2018-04-16 09:43:04 +0800

    更新

--- components/masonry/index.js
+++ components/masonry/index.js
@@ -2,10 +2,20 @@ import React, { PureComponent } from 'react';
 import Masonry from 'react-masonry-component';
 
 export default class MyMasonry extends PureComponent {
+  state = {
+    inited: false,
+  }
   render() {
     const { children } = this.props;
-    return (<Masonry>
-      {children}
+    const { inited } = this.state;
+    return (<Masonry
+      onImagesLoaded={() => {
+        if (!inited) {
+          this.setState({ inited: true });
+        }
+      }}
+    >
+      {inited && children}
     </Masonry>);
   }
 }
