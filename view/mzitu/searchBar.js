34b8e6199e81c837a88c64c96f47ed06808d163b
liumin1128 <970568830@qq.com>
2018-04-23 02:40:01 +0800

    更新

--- view/mzitu/searchBar.js
+++ view/mzitu/searchBar.js
@@ -1,6 +1,7 @@
 import React, { PureComponent } from 'react';
 import Router from 'next/router';
 import Search from '../../components/search';
+import { Divider } from 'material-ui';
 
 export default class SearchBar extends PureComponent {
   onSubmit = (value) => {
@@ -10,6 +11,6 @@ export default class SearchBar extends PureComponent {
     });
   }
   render() {
-    return (<Search onSubmit={this.onSubmit} />);
+    return <div style={{ marginBottom: 32 }}><Search onSubmit={this.onSubmit} /></div>;
   }
 }
