ec5e9a176ddf9392d8e1cf22495bf5e07c659b45
liumin1128 <970568830@qq.com>
2018-04-23 13:02:49 +0800

    更新

--- pages/_document.js
+++ pages/_document.js
@@ -28,6 +28,7 @@ class MyDocument extends Document {
             rel="stylesheet"
             // href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"
           />
+          <link rel="icon" href="/favicon.ico" />
           <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
 
         </Head>
