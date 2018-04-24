6a27b444f98c30d1ea95a1139cfaae2c55e89236
liumin1128 <970568830@qq.com>
2018-04-24 23:21:38 +0800

    更新

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,3 @@
-import React, { PureComponent } from 'react';
-import Grid from 'material-ui/Grid';
-import styleRoot from '../hoc/styleRoot';
-import apolloRoot from '../hoc/apolloRoot';
-import reduxRoot from '../hoc/reduxRoot';
-// import { queryProvider } from '../hoc/getQuery';
-import SaysList from '../view/says/list';
-import Layout from '../layout';
+import Index from './say';
 
-@reduxRoot
-@apolloRoot
-@styleRoot
-// @queryProvider
-export default class News extends PureComponent {
-  static async getInitialProps({ query }) {
-    return { query };
-  }
-  render() {
-    return (
-      <Layout>
-        <Grid container spacing={24}>
-          <Grid item xs={12} sm={12} md={8}>
-            <SaysList />
-          </Grid>
-          <Grid item xs={12} sm={12} md={4}>
-              2
-          </Grid>
-        </Grid>
-      </Layout>
-    );
-  }
-}
+export default Index;
--- pages/say/index.js
+++ pages/say/index.js
@@ -1,64 +1,32 @@
 import React, { PureComponent } from 'react';
-import { withStyles } from 'material-ui/styles';
 import Grid from 'material-ui/Grid';
 import styleRoot from '../../hoc/styleRoot';
 import apolloRoot from '../../hoc/apolloRoot';
 import reduxRoot from '../../hoc/reduxRoot';
+// import { queryProvider } from '../../hoc/getQuery';
 import SaysList from '../../view/says/list';
-
-const styles = () => ({
-  root: {
-    flexGrow: 1,
-    maxWidth: 1110,
-    margin: 'auto',
-    marginTop: 32,
-  },
-  body: {
-    maxWidth: 760,
-    width: '100%',
-    margin: '0 auto',
-  },
-  container: {
-    boxSizing: 'border-box',
-    margin: 0,
-    // border: '1px red solid',
-    width: '100%',
-    '@media (max-width: 960px)': {
-      margin: 0,
-    },
-  },
-});
+import Layout from '../../layout';
 
 @reduxRoot
 @apolloRoot
 @styleRoot
-@withStyles(styles)
+// @queryProvider
 export default class News extends PureComponent {
   static async getInitialProps({ query }) {
     return { query };
   }
-  // componentDidMount() {
-  //   console.log('xxx');
-  // }
   render() {
-    const { classes, query } = this.props;
     return (
-      <div className={classes.root}>
-        <div className={classes.root}>
-          <Grid className={classes.container} container spacing={24}>
-            <Grid item xs={12} sm={12} md={8}>
-              <div className={classes.body}>
-                <SaysList />
-              </div>
-            </Grid>
-            <Grid item xs={12} sm={12} md={4}>
-              <div className={classes.body}>
-                <h1>777</h1>
-              </div>
-            </Grid>
+      <Layout>
+        <Grid container spacing={24}>
+          <Grid item xs={12} sm={12} md={8}>
+            <SaysList />
           </Grid>
-        </div>
-      </div>
+          <Grid item xs={12} sm={12} md={4}>
+              2
+          </Grid>
+        </Grid>
+      </Layout>
     );
   }
 }
