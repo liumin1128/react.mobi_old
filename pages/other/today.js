6f5b6f59054545763ee932141e1131e66cff4ac6
liumin1128 <970568830@qq.com>
2018-05-04 00:23:23 +0800

    更新

---
+++ pages/other/today.js
@@ -0,0 +1,28 @@
+import React, { PureComponent } from 'react';
+import Grid from 'material-ui/Grid';
+import Typography from 'material-ui/Typography';
+import withRoot from '../../hoc';
+import Layout from '../../layout';
+import List from '../../components/calender/todayInHistory';
+
+@withRoot
+export default class TodayInHistory extends PureComponent {
+  // static async getInitialProps({ query }) {
+  //   return { query };
+  // }
+  render() {
+    return (
+      <Layout>
+        <Grid container spacing={24}>
+          <Grid item xs={12} sm={12} md={12}>
+            <Typography variant="headline" component="h1" color="textSecondary">
+              历史上的今天
+            </Typography>
+            <br />
+            <List />
+          </Grid>
+        </Grid>
+      </Layout>
+    );
+  }
+}
