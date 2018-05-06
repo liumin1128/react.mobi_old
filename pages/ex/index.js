b3fe12d561a4436cf9a839cb62fefaccc41bc053
liumin1128 <970568830@qq.com>
2018-05-06 23:44:28 +0800

    更新

---
+++ pages/ex/index.js
@@ -0,0 +1,28 @@
+import React, { PureComponent, Fragment } from 'react';
+import Grid from 'material-ui/Grid';
+import withRoot from '../../hoc';
+import List from '../../view/explore/list';
+import Layout from '../../layout';
+import Calender from '../../components/calender';
+import Appbar from '../../components/appbar';
+
+@withRoot
+export default class Says extends PureComponent {
+  render() {
+    return (
+      <Fragment>
+        <Appbar />
+        <Layout>
+          <Grid container spacing={24}>
+            <Grid item xs={12} sm={12} md={8}>
+              <List />
+            </Grid>
+            <Grid item xs={12} sm={12} md={4}>
+              <Calender />
+            </Grid>
+          </Grid>
+        </Layout>
+      </Fragment>
+    );
+  }
+}
---
+++ view/explore/list.js
@@ -0,0 +1,99 @@
+import React, { PureComponent, Fragment } from 'react';
+import Grid from 'material-ui/Grid';
+import Link from 'next/link';
+import { withStyles } from 'material-ui/styles';
+import Card, { CardActions, CardContent, CardMedia } from 'material-ui/Card';
+import Typography from 'material-ui/Typography';
+import Button from 'material-ui/Button';
+
+const styles = {
+  root: {
+    paddingBottom: 0,
+  },
+  card: {
+    marginBottom: 24,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    marginBottom: 8,
+    fontSize: 18,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+};
+
+@withStyles(styles)
+export default class MeizituDetail extends PureComponent {
+  render() {
+    const { classes } = this.props;
+    const list = [
+      {
+        title: '站长的blog',
+        content: '历史上的今天，数据来自网络',
+        cover: '',
+        url: 'http://liumin.me',
+      },
+      {
+        title: 'iReader ',
+        content: '基于react实现的电子书阅读器，数据来自追书神器',
+        cover: '',
+        url: 'http://ireader.liumin.me',
+      },
+      {
+        title: '爆笑Gif',
+        content: '多玩囧图，git板块，原版网页太卡...本页面使用了瀑布流，懒加载',
+        cover: '',
+        url: '/bxgif',
+      },
+      {
+        title: '妹子图',
+        content: '嘿嘿嘿',
+        cover: '',
+        url: '/meizitu',
+      },
+      {
+        title: '福利图',
+        content: '你懂得',
+        cover: '',
+        url: '/meizitu',
+      },
+      {
+        title: '历史上的今天',
+        content: '历史上的今天，数据来自网络',
+        cover: '',
+        url: '/other/today',
+      },
+    ];
+    return (<Fragment>
+      {
+        list.map(i => (<Card className={classes.card}>
+          <CardMedia
+            className={classes.media}
+            title="Contemplative Reptile"
+            style={{ paddingTop: i.cover ? '38%' : 0 }}
+          />
+          <CardContent className={classes.root}>
+            <Typography className={classes.title} variant="headline" component="h2">
+              {i.title}
+            </Typography>
+            <Typography color="textSecondary">
+              {i.content}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Link href={i.url} >
+              <a target="_blank"><Button size="small" color="primary">点击查看</Button></a>
+            </Link>
+          </CardActions>
+        </Card>))
+      }
+
+    </Fragment>);
+  }
+}
+
