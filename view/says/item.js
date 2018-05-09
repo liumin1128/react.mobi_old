135fd6d4fba49c60aefde3f92f3f449ac856aa5c
liumin1128 <970568830@qq.com>
2018-05-09 11:35:13 +0800

    更新

--- view/says/item.js
+++ view/says/item.js
@@ -16,7 +16,6 @@ import MoreVertIcon from '@material-ui/icons/MoreVert';
 import timeago from '../../utils/timeago';
 
 const styles = theme => ({
-  card: { marginBottom: theme.spacing.unit * 3 },
   media: {
     height: 250,
   },
