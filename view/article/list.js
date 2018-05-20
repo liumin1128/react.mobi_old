2d757bf5c7fed28aaa90d168989475077f9a29ed
liumin1128 <970568830@qq.com>
2018-05-20 22:55:24 +0800

    更新

--- view/article/list.js
+++ view/article/list.js
@@ -1,35 +1,28 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { graphql } from 'react-apollo';
 import { ARTICLE_LIST } from '@/graphql/article';
 import { fetchMore } from '@/graphql';
 import Item from './item';
 
 function PostList({ data, loadMore }) {
-  const {
-    loading, error, list, meta,
-  } = data;
+  const { loading, error, list, meta } = data;
 
   if (loading) return 'Loading...';
   if (error) return `Error! ${error.message}`;
+
   const isEnd = meta.count <= list.length;
 
-  return (<div>
+  return (<Fragment>
     {list.map(i => <Item key={i._id} {...i} />)}
     {!isEnd && <button onClick={() => loadMore()}>
       {loading ? 'Loading...' : 'Show More'}
     </button>}
-  </div>);
+  </Fragment>);
 }
-
-
-export const params = {
-  skip: 0,
-  first: 5,
-};
 
 export default graphql(ARTICLE_LIST, {
   options: {
-    variables: params,
+    variables: { skip: 0, first: 5 },
   },
   props: ({ data }) => ({
     data,
