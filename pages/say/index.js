b7a75d14373710c284ac0d9d8673ae42a1c319bb
liumin1128 <970568830@qq.com>
2018-05-20 13:17:40 +0800

    gx

--- pages/say/index.js
+++ pages/say/index.js
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react';
+import React, { PureComponent, Fragment } from 'react';
 import Grid from '@material-ui/core/Grid';
 import List from '@/view/says/list';
 import Layout from '@/components/layout';
@@ -8,17 +8,19 @@ import Appbar from '@/components/appbar';
 export default class Says extends PureComponent {
   render() {
     return (
-      <Layout>
+      <Fragment>
         <Appbar />
-        <Grid container spacing={24}>
-          <Grid item xs={12} sm={12} md={8}>
-            <List />
+        <Layout>
+          <Grid container spacing={24}>
+            <Grid item xs={12} sm={12} md={8}>
+              <List />
+            </Grid>
+            <Grid item xs={12} sm={12} md={4}>
+              <Calender />
+            </Grid>
           </Grid>
-          <Grid item xs={12} sm={12} md={4}>
-            <Calender />
-          </Grid>
-        </Grid>
-      </Layout>
+        </Layout>
+      </Fragment>
     );
   }
 }
