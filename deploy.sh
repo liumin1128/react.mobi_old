ce53aab74c4667b829320d3ae750166547b3e07a
liumin1128 <970568830@qq.com>
2018-05-21 16:00:32 +0800

    更新

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,9 @@
 # scp -r /Users/liumin/Desktop/Dva/Admin.HuarenLife/dist root@45.79.93.51:/root/huarenShenghuo
 # DATA=$(date +%y%m%d)
 
-# SERVER="root@45.79.93.51"
-DIR="/root/react.mobi"
-# LOCAL_DIR="pages/*"
+SERVER="root@47.94.150.105"
+DIR="/root/react.mobi/.next"
+LOCAL_DIR=".next"
 
 # FILELIST=$(git diff --name-only HEAD~ HEAD)
 
@@ -14,10 +14,10 @@ DIR="/root/react.mobi"
 # done
 
 echo '本地路径：'
-# echo $(pwd)/${LOCAL_DIR}
-# echo $(pwd)
+echo $(pwd)/${LOCAL_DIR}
+echo $(pwd)
 
-TEMP=`find $(pwd) \( -path $(pwd)/node_modules -o -path $(pwd)/.next -o -path $(pwd)/.git \) -prune -o -maxdepth 1 -mindepth 1 -name "*" -print`
+# TEMP=`find $(pwd) \( -path $(pwd)/node_modules -o -path $(pwd)/.next -o -path $(pwd)/.git \) -prune -o -maxdepth 1 -mindepth 1 -name "*" -print`
 # TEMP=`find $(pwd) -name "*" -path $(pwd)/node_modules -prune -o -maxdepth 1 -print`
 # TEMP=`find $(pwd)/* -path $(pwd)/node_modules -prune -o -maxdepth 0 -print`
 
@@ -26,10 +26,10 @@ echo '远程路径：'
 echo ${DIR}
 
 
-echo '开始推送文件到国内服务器'
-scp -r ${TEMP} root@123.206.49.55:${DIR}
+# echo '开始推送文件到国内服务器'
+# scp -r ${TEMP} root@123.206.49.55:${DIR}
 
-# echo '开始推送文件到国外服务器'
-# scp -r ${TEMP} root@45.79.93.51:${DIR}
+echo '开始推送文件到坚挺号服务器'
+scp -r $(pwd)/${LOCAL_DIR} ${SERVER}:${DIR}
 
 echo '传输完毕，cool~~~'
