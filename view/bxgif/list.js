94d136e67130c20348dd9355b3ac3e20e2255574
liumin1128 <970568830@qq.com>
2018-09-11 14:51:27 +0800

    更新

--- view/bxgif/list.js
+++ view/bxgif/list.js
@@ -1,15 +1,35 @@
 import React, { PureComponent, Fragment } from 'react';
 import Masonry from 'react-masonry-component';
 import Grid from '@material-ui/core/Grid';
+import { withStyles } from '@material-ui/core/styles';
+
 import hocNossr from '@/hoc/hocNossr';
 import { BXGIF_LIST } from '@/graphql/schema/bxgif';
 import { listQuery } from '@/graphql/utils';
+
 import Item from './item';
 
+const styles = theme => ({
+  list: {
+    [theme.breakpoints.down('xs')]: {
+      margin: theme.spacing.unit * 0.5,
+    },
+  },
+  item: {
+    padding: theme.spacing.unit * 1,
+    width: '100%',
+    display: 'block',
+    [theme.breakpoints.down('xs')]: {
+      padding: theme.spacing.unit * 0.5,
+    },
+  },
+});
+
 @hocNossr(listQuery(BXGIF_LIST))
+@withStyles(styles)
 export default class test extends PureComponent {
   render() {
-    const { data = {} } = this.props;
+    const { data = {}, classes } = this.props;
     const { loading = true, error, list = [] } = data;
 
     if (loading) return 'Loading...';
@@ -17,15 +37,11 @@ export default class test extends PureComponent {
 
     return (
       <Fragment>
-        <Masonry>
+        <Masonry className={classes.list}>
           {list.map(i => (
             <Grid
               key={i._id}
-              style={{
-                padding: '8px',
-                width: '100%',
-                display: 'block',
-              }}
+              className={classes.item}
               item
               xs={6}
               sm={4}
