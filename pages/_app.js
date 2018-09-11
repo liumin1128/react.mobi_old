0a9398da7ef7f330922726c159d661f854cbf5fa
liumin1128 <970568830@qq.com>
2018-09-11 16:18:55 +0800

    优化pageContext

--- pages/_app.js
+++ pages/_app.js
@@ -2,15 +2,20 @@ import React from 'react';
 import App, { Container } from 'next/app';
 import { ApolloProvider } from 'react-apollo';
 import hoc from '@/hoc';
+import { isServerSide } from '@/utils/common';
 
 @hoc
 export default class MyApp extends App {
   render() {
-    const { Component, pageProps, pageContext, apolloClient } = this.props;
+    const { Component, pageProps, pageContext, apolloClient, router, ...other } = this.props;
     return (
       <Container>
         <ApolloProvider client={apolloClient}>
-          <Component pageContext={pageContext} {...pageProps} />
+          {
+            isServerSide()
+              ? <Component pageContext={pageContext} router={router} {...pageProps} />
+              : <Component router={router} {...pageProps} />
+          }
         </ApolloProvider>
       </Container>
     );
