e4c6a3671adac3736194c829f96002cc32026339
liumin1128 <970568830@qq.com>
2018-09-11 11:27:38 +0800

    新增nossr hoc

--- view/test.js
+++ view/test.js
@@ -1,20 +1,20 @@
 import React, { PureComponent, Fragment } from 'react';
-import nossr from '@/hoc/nossr';
+import hocNossr from '@/hoc/hocNossr';
 import { BXGIF_LIST } from '@/graphql/schema/bxgif';
 import { listQuery } from '@/graphql/utils';
 
-@nossr
-@listQuery(BXGIF_LIST)
+@hocNossr(listQuery(BXGIF_LIST))
 export default class test extends PureComponent {
   render() {
-    const { data } = this.props;
-    const { loading, error, list } = data;
+    const { data = {} } = this.props;
+    const { loading, error, list = [] } = data;
 
     if (loading) return 'Loading...';
     if (error) return `Error! ${error.message}`;
 
     return (
       <Fragment>
+      666
         {list.map(i => (
           <div id={i._id}>
             {i.title}
