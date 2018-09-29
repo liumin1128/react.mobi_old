8115aafef7905d0be92938d441c3611d271ed142
liumin1128 <970568830@qq.com>
2018-09-29 17:46:47 +0800

    更新

--- view/mzitu/list.js
+++ view/mzitu/list.js
@@ -1,11 +1,12 @@
 import React, { PureComponent, Fragment } from 'react';
 import { withStyles } from '@material-ui/core/styles';
+import { Query } from 'react-apollo';
 import Waypoint from 'react-waypoint';
 import Grid from '@material-ui/core/Grid';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import Item from './item';
 import { MZITU_LIST } from '@/graphql/schema/mzitu';
-import { listQuery } from '@/graphql/utils';
+import { updateQuery } from '@/graphql/utils';
 
 const styles = theme => ({
   list: {
@@ -27,35 +28,59 @@ const styles = theme => ({
   },
 });
 
-@listQuery(MZITU_LIST)
 @withStyles(styles)
 export default class MzituList extends PureComponent {
   render() {
-    const { data = {}, classes, fetchMore } = this.props;
-    const { loading = true, error, list = [] } = data;
-
-    if (loading) return 'Loading...';
-    if (error) return `Error! ${error.message}`;
-
+    const { query = {}, classes } = this.props;
+    const { search, tag } = query;
     return (
       <Fragment>
-        <Grid container spacing={16}>
+        <Query
+          query={MZITU_LIST}
+          variables={{ search, tag }}
+        >
 
-          {list.map(i => (
-            <Grid
-              key={i._id}
-              className={classes.item}
-              item
-              xs={6}
-              sm={4}
-            >
-              <Item {...i} />
-            </Grid>
-          ))}
-        </Grid>
+          {({ loading, error, data = {}, fetchMore }) => {
+            if (loading) return 'Loading...';
+            if (error) return `Error! ${error.message}`;
 
-        <Waypoint onEnter={fetchMore} />
-        <CircularProgress color="secondary" className={classes.progress} />
+            const { list = [] } = data;
+
+            const loadMore = () => fetchMore({
+              variables: {
+                page: (Math.floor(list.length / 24) || 1) + 1,
+                search,
+              },
+              updateQuery,
+            });
+
+            return (<Fragment>
+              {' '}
+              <Grid container spacing={16}>
+
+                {list.map(i => (
+                  <Grid
+                    key={i._id}
+                    className={classes.item}
+                    item
+                    xs={6}
+                    sm={4}
+                  >
+                    <Item {...i} />
+                  </Grid>
+                ))}
+              </Grid>
+
+              <Waypoint onEnter={loadMore} />
+              <CircularProgress
+                color="secondary"
+                className={classes.progress}
+              />
+            </Fragment>
+            );
+          }}
+        </Query>
+
       </Fragment>
     );
   }
