c18d288c5cc8fea726cb8472bd4fb01e5e9859ce
liumin1128 <970568830@qq.com>
2018-10-07 16:21:19 +0800

    更新

--- pages/login/index.js
+++ pages/login/index.js
@@ -1,7 +1,7 @@
 import React, { PureComponent } from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import Dialog from '@material-ui/core/Dialog';
-import Card from '@material-ui/core/Card';
+// import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Login from '@/view/login';
