1f4dc6d99a643aa6bbf1cba32fc64cd49e910e81
liumin1128 <970568830@qq.com>
2018-10-12 11:50:45 +0800

    创建say页面

---
+++ pages/say/index.js
@@ -0,0 +1,14 @@
+import React, { PureComponent } from 'react';
+import Layout from '@/components/Layout';
+
+export default class index extends PureComponent {
+  render() {
+    return (
+      <div>
+        <Layout>
+          say
+        </Layout>
+      </div>
+    );
+  }
+}
