cbaa035a0b4bf86965f3230f81b2d12478079379
liumin1128 <970568830@qq.com>
2018-10-18 15:38:54 +0800

    使用新的异步组件

--- components/form/RichEditor.js
+++ components/form/RichEditor.js
@@ -1,19 +1,23 @@
 import React, { PureComponent, createRef } from 'react';
-import dynamic from 'next/dynamic';
 import { getJSON, getHTML } from '@/components/DraftEditor/utils';
 import CircularProgress from '@material-ui/core/CircularProgress';
 
-const DynamicDraftEditorWithNoSSR = dynamic(() => import('@/components/DraftEditor'), {
-  ssr: false,
-  loading: () => (
-    <div style={{ padding: 16, minHeight: 302 }}>
-      <CircularProgress size={20} style={{ margin: 'auto', display: 'block' }} />
-    </div>
-  ),
-});
-
 export default class RichEditor extends PureComponent {
-  editor = createRef()
+  constructor(props) {
+    super(props);
+    this.state = {
+      DynamicComponent: null,
+    };
+    this.editor = createRef();
+  }
+
+  componentDidMount() {
+    // if your component use export default, use val.default,
+    // else use val[yourComponentName]
+    import('@/components/DraftEditor')
+      .then(val => this.setState({ DynamicComponent: val.default }));
+  }
+
 
   getJSON = () => {
     const { editorState } = this.editor.state;
@@ -26,18 +30,19 @@ export default class RichEditor extends PureComponent {
   }
 
   render() {
-    // const { input: { onChange } } = this.props;
+    const { DynamicComponent } = this.state;
+    if (!DynamicComponent) {
+      return (
+        <div style={{ padding: 16, minHeight: 302 }}>
+          <CircularProgress
+            size={20}
+            style={{ margin: 'auto', display: 'block' }}
+          />
+        </div>
+      );
+    }
     return (
-      <DynamicDraftEditorWithNoSSR
-        ref={(c) => { this.editor = c; }}
-        // _onChange={(editorState) => {
-        //   console.log(this.editor.state);
-        //   onChange({
-        //     json: JSON.stringify(getJSON(editorState)),
-        //     html: getHTML(editorState),
-        //   });
-        // }}
-      />
+      <DynamicComponent ref={(c) => { this.editor = c; }} />
     );
   }
 }
