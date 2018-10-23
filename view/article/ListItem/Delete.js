4e5b3f4ebd76c53a03d82269e354800abd85c2be
liumin1128 <970568830@qq.com>
2018-10-23 23:44:39 +0800

    删除并提醒

--- view/article/ListItem/Delete.js
+++ view/article/ListItem/Delete.js
@@ -2,15 +2,18 @@ import React, { PureComponent } from 'react';
 import MenuItem from '@material-ui/core/MenuItem';
 import { graphql } from 'react-apollo';
 import { DELETE_ARTICLE } from '@/graphql/schema/article';
+import Snackbar from '@/components/snackbar';
 
 @graphql(DELETE_ARTICLE)
 export default class Delete extends PureComponent {
   handleClose = async () => {
     try {
       const { id, mutate } = this.props;
-      const data = await mutate({ variables: { id }, refetchQueries: 'ArticleList' });
-      console.log('data');
-      console.log(data);
+      const { data: { result: { status, message } } } = await mutate({
+        variables: { id },
+        refetchQueries: ['ArticleList'],
+      });
+      Snackbar.success(`[${status}]${message}`);
     } catch (error) {
       console.log(error);
     }
