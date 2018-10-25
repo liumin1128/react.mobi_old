2e3c2d78f0ce8b0aab74bc656e772c3be7ce1238
liumin1128 <970568830@qq.com>
2018-10-25 23:22:14 +0800

    修复输入框样式

--- components/form/textField.js
+++ components/form/textField.js
@@ -5,14 +5,19 @@ export default ({
   input: { name, onChange, value, ...restInput },
   meta,
   ...rest
-}) => (
-  <TextField
-    {...rest}
-    name={name}
-    helperText={meta.touched ? meta.error : undefined}
-    error={meta.error && meta.touched}
-    inputProps={restInput}
-    onChange={onChange}
-    value={value}
-  />
-);
+}) => {
+  // 加了restInput，会导致样式异常，无法focus
+  // console.log('restInput');
+  // console.log(restInput);
+  return (
+    <TextField
+      {...rest}
+      name={name}
+      helperText={meta.touched ? meta.error : undefined}
+      error={meta.error && meta.touched}
+      // inputProps={restInput}
+      onChange={onChange}
+      value={value}
+    />
+  );
+};
