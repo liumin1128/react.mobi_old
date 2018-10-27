4bad51692192a32caf67d4b760a5173c8a66f000
liumin1128 <970568830@qq.com>
2018-10-27 21:50:54 +0800

    更新评论组件

--- view/comments/list.js
+++ view/comments/list.js
@@ -3,7 +3,7 @@ import { withStyles } from '@material-ui/core/styles';
 import { Query } from 'react-apollo';
 import { COMMENT_LIST } from '@/graphql/schema/comment';
 import Paper from '@material-ui/core/Paper';
-import ButtonBase from '@material-ui/core/ButtonBase';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import CardContent from '@material-ui/core/CardContent';
 import Avatar from '@material-ui/core/Avatar';
 import Typography from '@material-ui/core/Typography';
@@ -40,92 +40,97 @@ const styles = theme => ({
   nickname: {
     fontSize: 14,
   },
-  pageBox: {
-    display: 'flex',
-    justifyContent: 'center',
-  },
-  pageBtn: {
-    minHeight: 40,
-    lineHeight: '40px',
-    textAlign: 'center',
-    minWidth: 40,
-    display: 'inline-block',
-  },
 });
 
 @withStyles(styles)
 export default class test extends PureComponent {
-  renderPagination = ({ onChange }) => {
-    const { classes, _id } = this.props;
+  state = {
+    page: 1,
+  }
+
+  renderHeader = ({ count, loading }) => {
+    const { classes } = this.props;
     return (
-      <div className="pagination">
-        {
-        [1, 2, 3, 4].map(i => (
-          <ButtonBase
-            onClick={() => {
-              onChange(i);
-            }}
-            className={classes.pageBtn}
-          >
-            {i}
-          </ButtonBase>
-        ))
-      }
-      </div>
+      <CardContent className={classes.header}>
+        <Typography className={classes.title} variant="h6" component="h6">
+          {loading ? 'loading' : (count === 0 ? '暂无评论' : `${count} 条评论`)}
+        </Typography>
+        <Typography>时间 | 热度</Typography>
+      </CardContent>
+    );
+  }
+
+  renderList = ({ list, loading }) => {
+    const { classes } = this.props;
+    if (loading) {
+      return (
+        <div style={{ padding: 16, minHeight: 100 }}>
+          <CircularProgress
+            size={20}
+            style={{ margin: 'auto', display: 'block' }}
+          />
+        </div>
+      );
+    }
+    return (
+      <Fragment>
+        {list.map(i => (
+          <Fragment key={i._id}>
+            <div className={classes.flex}>
+              <Avatar
+                aria-label="Recipe"
+                className={classes.avatar}
+                src={i.user.avatarUrl}
+                size={24}
+              >
+                {i.user.nickname}
+              </Avatar>
+              <Typography variant="h6" className={classes.nickname}>{i.user.nickname}</Typography>
+            </div>
+            <p>{i.content}</p>
+          </Fragment>
+        ))}
+      </Fragment>
+    );
+  }
+
+  renderPagination = ({ count, refetch }) => {
+    const { page } = this.state;
+    return (
+      <Pagination
+        page={page}
+        pageSize={5}
+        total={count}
+        onChange={(val) => {
+          this.setState({ page: val }, () => {
+            refetch({ skip: (val - 1) * 5 });
+          });
+        }}
+      />
     );
   }
 
   render() {
     const { classes, _id } = this.props;
+    const { page } = this.state;
+
     return (
-      <Query query={COMMENT_LIST} variables={{ commentTo: _id }}>
+      <Query query={COMMENT_LIST} variables={{ commentTo: _id, skip: (page - 1) * 5 }}>
         {({ loading, error, data = {}, refetch }) => {
           // todo 将refetch 传到create中
-          const { list = [], meta } = data;
-          if (loading) return 'Loading...';
+          const { list = [], meta = { } } = data;
+          // if (loading) return 'Loading...';
           if (error) return 'error';
           return (
             <Paper className={classes.root}>
-              <CardContent className={classes.header}>
-                <Typography className={classes.title} variant="h6" component="h6">
-                  {meta.count === 0 ? '暂无评论' : `${meta.count} 条评论`}
-                </Typography>
-                <Typography>时间 | 热度</Typography>
+              {this.renderHeader({ count: meta.count, loading })}
+              <Divider light />
+              <CardContent>
+                {this.renderList({ list, loading })}
+                {this.renderPagination({ count: meta.count, refetch })}
               </CardContent>
               <Divider light />
               <CardContent>
-                <Fragment>
-                  {list.map(i => (
-                    <Fragment key={i._id}>
-                      <div className={classes.flex}>
-                        <Avatar
-                          aria-label="Recipe"
-                          className={classes.avatar}
-                          src={i.user.avatarUrl}
-                          size={24}
-                        >
-                          {i.user.nickname}
-                        </Avatar>
-                        <Typography variant="h6" className={classes.nickname}>{i.user.nickname}</Typography>
-                      </div>
-                      <p>{i.content}</p>
-                    </Fragment>
-                  ))}
-                </Fragment>
-
-                <Pagination
-                  page={1}
-                  pageSize={5}
-                  total={meta.count}
-                  onChange={(page) => {
-                    refetch({ skip: (page - 1) * 5 });
-                  }}
-                />
-              </CardContent>
-
-              <Divider light />
-              <CardContent>
-
                 <Create _id={_id} refetch={refetch} />
               </CardContent>
             </Paper>
