603f249a9d56c1f53b2f850114653d5b9d49ef15
liumin1128 <970568830@qq.com>
2018-10-29 00:46:47 +0800

    更新

---
+++ auto_build.sh
@@ -0,0 +1,27 @@
+PRO_DIR="../react.mobi"
+
+# echo "进入项目目录"
+PATH_OLD=`pwd`
+cd $PRO_DIR
+PATH_NEW=`pwd`
+echo "从 $PATH_OLD 切换到 $PATH_NEW"
+
+# source ~/.nvm/nvm.sh
+# nvm use 8.9.0
+# echo "切换到8.9.0"
+echo "Node 版本："
+node -v
+# echo "cd $PRO_DIR"
+
+echo "正在从git同步代码"
+git fetch --all
+git reset --hard origin/master
+git pull
+
+# echo "安装依赖"
+# yarn
+
+echo "重启pm2"
+# pm2 restart api.react.mobi
+yarn pm2
+echo "finished----------"
