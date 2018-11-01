b1fde3016c8b73006b3adcb9288054bfd9f68efc
liumin1128 <970568830@qq.com>
2018-11-01 23:32:21 +0800

    更新

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,24 @@
-import Index from './article';
+import React, { PureComponent } from 'react';
+import Hidden from '@material-ui/core/Hidden';
+import Grid from '@material-ui/core/Grid';
+import Article from '@/view/article/list';
+import Side from '@/view/article/side';
 
-export default Index;
+export default class index extends PureComponent {
+  render() {
+    return (
+      <div>
+        <Grid container spacing={16}>
+          <Grid item xs={12} sm={12} md={8}>
+            <Article />
+          </Grid>
+          <Grid item xs={12} sm={12} md={4}>
+            <Hidden implementation="css" only={[ 'sm', 'xs' ]}>
+              <Side />
+            </Hidden>
+          </Grid>
+        </Grid>
+      </div>
+    );
+  }
+}
