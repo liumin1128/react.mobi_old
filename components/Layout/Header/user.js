152ac3f23f060cc484d8eaadf68050145538dee9
liumin1128 <970568830@qq.com>
2018-11-12 14:59:46 +0800

    更新

--- components/Layout/Header/user.js
+++ components/Layout/Header/user.js
@@ -3,17 +3,12 @@ import { Query, graphql } from 'react-apollo';
 import dynamic from 'next/dynamic';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
 import { withStyles } from '@material-ui/core/styles';
 import { USERINFO, USER_LOGIN } from '@/graphql/schema/user';
-import modal from '@/hoc/modal';
-import { USER_TOKEN } from '@/config/base';
-import { setStorage } from '@/utils/store';
 
-// const DynamicComponentWithCustomLoading = dynamic(() => import('@/view/login/form'), {
-//   loading: () => <p>...</p>,
-// });
+const DynamicComponentWithCustomLoading = dynamic(() => import('@/view/login/modal'), {
+  loading: () => <p>...</p>,
+});
 
 const styles = theme => ({
   paper: {
@@ -24,50 +19,35 @@ const styles = theme => ({
 @withStyles(styles)
 @graphql(USER_LOGIN)
 export default class user extends PureComponent {
-  refetch = () => {}
-
-  login = () => {
-    const { classes } = this.props;
-    modal(this.renderLoginForm, {
-      classes: {
-        paper: classes.paper,
-      },
-    });
+  state = {
+    showLoginModal: true,
   }
 
-  onSubmit = async (values, close) => {
-    try {
-      const { mutate } = this.props;
-      const { data: { result: data } } = await mutate({
-        variables: values,
-        // refetchQueries: ['userInfo'],
-      });
-      if (data.status === 200) {
-        await setStorage(USER_TOKEN, data.token);
-        await this.refetch();
-        await close();
-      }
-    } catch (error) {
-      console.log('error');
-      console.log(error);
-    }
+  // onSubmit = async (values, close) => {
+  //   try {
+  //     const { mutate } = this.props;
+  //     const { data: { result: data } } = await mutate({
+  //       variables: values,
+  //       // refetchQueries: ['userInfo'],
+  //     });
+  //     if (data.status === 200) {
+  //       await setStorage(USER_TOKEN, data.token);
+  //       await this.refetch();
+  //       await close();
+  //     }
+  //   } catch (error) {
+  //     console.log('error');
+  //     console.log(error);
+  //   }
+  // }
+
+  renderLoginForm() {
+    return (
+      <DynamicComponentWithCustomLoading />
+    );
   }
 
-  renderLoginForm = ({ close }) => (
-    <Fragment>
-      <CardMedia
-        style={{ height: 0, paddingTop: '60%' }}
-        image={'https://imgs.react.mobi/FiIH1AWT8r5hJja50xiBSClwFvek'}
-      />
-      <CardContent>
-        <DynamicComponentWithCustomLoading
-          onSubmit={values => this.onSubmit(values, close)}
-        />
-      </CardContent>
-    </Fragment>
-  )
-
-  render() {
+  renderUser() {
     return (
       <Query query={USERINFO} errorPolicy="all">
         {({ loading, error, data = {}, refetch }) => {
@@ -86,7 +66,7 @@ export default class user extends PureComponent {
                   style={{ background: 'rgba(255,255,255,0.1)' }}
                   onClick={this.login}
                 >
-登录
+                  登录
                 </Button>
               </Fragment>
             );
@@ -99,6 +79,16 @@ export default class user extends PureComponent {
           );
         }}
       </Query>
+    );
+  }
+
+  render() {
+    const { showLoginModal } = this.state;
+    return (
+      <Fragment>
+        {this.renderUser()}
+        {showLoginModal && this.renderLoginForm()}
+      </Fragment>
     );
   }
 }
---
+++ view/login/modal.js
@@ -0,0 +1,15 @@
+import React, { PureComponent } from 'react';
+
+export default class LoginModal extends PureComponent {
+  state = {
+    mode: 'up',
+  }
+
+  render() {
+    return (
+      <div>
+        8
+      </div>
+    );
+  }
+}
