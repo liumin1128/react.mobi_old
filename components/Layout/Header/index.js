be771c6feaf960251328eec2dff8822cbf8c4b1f
liumin1128 <970568830@qq.com>
2018-11-20 21:01:37 +0800

    更新

--- components/Layout/Header/index.js
+++ components/Layout/Header/index.js
@@ -1,12 +1,11 @@
-import React, { PureComponent } from 'react';
+import React, { PureComponent, Fragment } from 'react';
 import dynamic from 'next/dynamic';
 import Link from 'next/link';
+import Headroom from 'react-headroom';
 import { withStyles } from '@material-ui/core/styles';
 import Toolbar from '@material-ui/core/Toolbar';
 import AppBar from '@material-ui/core/AppBar';
 import Hidden from '@material-ui/core/Hidden';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
 import NavTabs from './NavTabs';
 import Logo from './logo';
 
@@ -36,26 +35,26 @@ export default class Header extends PureComponent {
   render() {
     const { classes } = this.props;
     return (
-      <div className={classes.root}>
-        <AppBar className={classes.root}>
-          <Toolbar>
-            <Link href="/">
-              <a><Logo /></a>
-            </Link>
-            <div className={classes.grow}>
-              <Hidden implementation="css" xsDown>
-                <NavTabs mode="large" />
-              </Hidden>
-            </div>
-
-            <DynamicUserInfoWithNoSSR />
-          </Toolbar>
-          <Hidden implementation="css" smUp>
-            <NavTabs />
-          </Hidden>
-        </AppBar>
-
-      </div>
+      <Fragment>
+        <Headroom style={{ height: 64 }}>
+          <AppBar position="static" className={classes.root}>
+            <Toolbar>
+              <Link href="/">
+                <a><Logo /></a>
+              </Link>
+              <div className={classes.grow}>
+                <Hidden implementation="css" xsDown>
+                  <NavTabs mode="large" />
+                </Hidden>
+              </div>
+              <DynamicUserInfoWithNoSSR />
+            </Toolbar>
+            <Hidden implementation="css" smUp>
+              <NavTabs />
+            </Hidden>
+          </AppBar>
+        </Headroom>
+      </Fragment>
     );
   }
 }
