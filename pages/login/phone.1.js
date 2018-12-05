9e13b60835e1e9ac336d4c258063ee6edf696d83
liumin1128 <970568830@qq.com>
2018-12-05 18:14:48 +0800

    更新

---
+++ pages/login/phone.1.js
@@ -0,0 +1,80 @@
+import React, { PureComponent, Fragment } from 'react';
+import Link from 'next/link';
+import { withStyles } from '@material-ui/core/styles';
+import Dialog from '@material-ui/core/Dialog';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Login from '@/view/login/phone';
+import Oauth from '@/view/login/oauth';
+import AppBar from '@/components/Layout/Header/SimpleAppbar';
+
+const styles = theme => ({
+  root: {
+    background: 'none',
+    marginTop: 64,
+  },
+  card: {
+    maxWidth: 360,
+    margin: '16px auto',
+    boxShadow: 'none',
+  },
+  media: {
+    height: 0,
+    paddingTop: '60%',
+  },
+  help: {
+    // background: 'red',
+    paddingTop: 0,
+    fontSize: 12,
+    color: '#666',
+    display: 'flex',
+    justifyContent: 'space-between',
+    '&  a': {
+      color: '#666',
+      textDecoration: 'none',
+    },
+  },
+});
+
+@withStyles(styles)
+export default class LoginPage extends PureComponent {
+  render() {
+    const { classes } = this.props;
+    return (
+      <Fragment>
+        <AppBar title="手机验证码登录" />
+        <Dialog
+          hideBackdrop
+          open
+          scroll="body"
+          // className={classes.card}
+          classes={{
+            paper: classes.card,
+            root: classes.root,
+          }}
+        >
+          <CardMedia
+            className={classes.media}
+            image={'https://imgs.react.mobi/FiIH1AWT8r5hJja50xiBSClwFvek'}
+          />
+          <CardContent>
+            <Login />
+          </CardContent>
+          <CardContent className={classes.help}>
+            <span>
+              还没有账号？
+              <Link href="/login/register">立即注册</Link>
+            </span>
+            <span>
+              <Link href="/login">用户名密码登录</Link>
+            </span>
+          </CardContent>
+
+          <CardContent style={{ background: 'rgba(0,0,0,0.02)', padding: 8 }}>
+            <Oauth />
+          </CardContent>
+        </Dialog>
+      </Fragment>
+    );
+  }
+}
--- view/login/phone.js
+++ view/login/phone.js
@@ -12,155 +12,33 @@ import Snackbar from '@/components/Snackbar';
 import { isPhoneNumber } from '@/utils/validate';
 import { setStorage } from '@/utils/store';
 import { USER_TOKEN } from '@/config/base';
-
 import SelectField from './components/SelectField';
 import CodeBtn from './components/CodeBtn';
+import PhoneLogin from './components/Form/PhoneLogin';
 
-const validate = (values) => {
-  const errors = {};
-
-  if (!values.countryCode) {
-    errors.countryCode = '区号不能为空';
-  }
-  if (!values.purePhoneNumber) {
-    errors.purePhoneNumber = '手机号不能为空';
-  } else if (!isPhoneNumber(values.countryCode, values.purePhoneNumber)) {
-    console.log(isPhoneNumber(values.countryCode, values.purePhoneNumber));
-    errors.purePhoneNumber = '手机号格式不正确';
-  }
-
-  if (!values.code) {
-    errors.code = '验证码不能为空';
-  }
-
-  console.log(errors);
-  return errors;
-};
-
-const styles = (theme) => {
-  return {
-    item: {
-      marginBottom: theme.spacing.unit * 3,
-    },
-  };
-};
-
-@withStyles(styles)
 export default class LoginForm extends PureComponent {
   render() {
-    const formData = {
-      // nickname: '本王今年八岁',
-      countryCode: '+86',
-      // purePhoneNumber: '18629974148',
-      // code: '434772',
-    };
-
-    const { classes, onLoginSuccess } = this.props;
     return (
       <Mutation mutation={USER_LOGIN_BY_PHONENUMBER_CODE}>
         {(mutation, { loading, error, data = {} }) => {
           const onRegister = async (values) => {
             try {
-              // console.log('values');
-              // console.log(values);
-
               const { data: { result: { status, message, token } } } = await mutation({
                 variables: values,
                 // refetchQueries: ['ArticleList'],
               });
-
               if (status === 200) {
                 await setStorage(USER_TOKEN, token);
-                if (onLoginSuccess) {
-                  await onLoginSuccess();
-                } else {
-                  window.location.href = '/';
-                }
               }
-
               Snackbar.success(message);
-
-              // Snackbar.success(`[${status}]${message}`);
             } catch (err) {
               Snackbar.success('登录失败');
-
               console.log('err');
               console.log(err);
             }
           };
-
-          return (
-            <Form
-              onSubmit={onRegister}
-              initialValues={formData}
-              validate={validate}
-              render={({ handleSubmit, values, valid, dirty, ...other }) => {
-                return (
-                  <form id="createArticleForm" onSubmit={handleSubmit}>
-
-                    <Grid container spacing={16}>
-                      <Grid item xs>
-                        <Field
-                          fullWidth
-                          key="countryCode"
-                          name="countryCode"
-                          label="国家"
-                          // value={10}
-                          className={classes.item}
-                          component={SelectField}
-                          type="text"
-                        />
-                      </Grid>
-                      <Grid item xs={8}>
-                        <Field
-                          fullWidth
-                          key="purePhoneNumber"
-                          name="purePhoneNumber"
-                          label="手机号"
-                          className={classes.item}
-                          component={TextField}
-                          type="text"
-                        />
-                      </Grid>
-                    </Grid>
-
-                    <Field
-                      fullWidth
-                      key="code"
-                      name="code"
-                      label="验证码"
-                      className={classes.item}
-                      component={TextField}
-                      type="text"
-                      InputProps={{
-                        endAdornment: (
-                          <InputAdornment variant="filled" position="end">
-                            <CodeBtn values={values} />
-                          </InputAdornment>
-                        ),
-                      }}
-                    />
-
-                    <Button
-                      variant="contained"
-                      size="large"
-                      color="primary"
-                      type="submit"
-                      style={{ marginRight: 16 }}
-                      disabled={!dirty && !valid}
-                      fullWidth
-                    >
-                      {loading && <CircularProgress style={{ marginRight: 8 }} color="inherit" size={14} thickness={5} />}
-                      登录
-                    </Button>
-
-                  </form>
-                );
-              }}
-            />
-          );
+          return <PhoneLogin onSubmit={onRegister} loading={loading} />;
         }}
-
       </Mutation>
 
     );
