c72a1b1081a9e828914568376f7d88d224fc343e
liumin1128 <970568830@qq.com>
2018-12-05 18:21:16 +0800

    更新

---
+++ view/login/components/Form/Login.js
@@ -0,0 +1,90 @@
+import React, { PureComponent } from 'react';
+import { Form, Field } from 'react-final-form';
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import TextField from '@/components/Form/TextField';
+import CircularProgress from '@material-ui/core/CircularProgress';
+
+const registerValidate = (values) => {
+  const errors = {};
+  if (!values.username) {
+    errors.username = '用户名不能为空';
+  }
+  if (!values.password) {
+    errors.password = '密码不能为空';
+  }
+  console.log(errors);
+  return errors;
+};
+
+const styles = (theme) => {
+  return {
+    item: {
+      marginBottom: theme.spacing.unit * 3,
+    },
+    button: {
+      height: 48,
+      borderRadius: 6,
+      boxShadow: '0 8px 30px rgba(0,0,0,0.1)',
+    },
+  };
+};
+
+@withStyles(styles)
+export default class LoginForm extends PureComponent {
+  render() {
+    const { onSubmit, loading, classes } = this.props;
+
+    return (
+      <Form
+        onSubmit={onSubmit}
+        validate={registerValidate}
+        render={({ handleSubmit, values, valid, dirty, ...other }) => {
+          // console.log('other');
+          // console.log(other);
+          return (
+            <form id="createArticleForm" onSubmit={handleSubmit}>
+
+              <Field
+                key="username"
+                name="username"
+                label="用户名"
+                placeholder="请输入手机号或邮箱"
+                className={classes.item}
+                component={TextField}
+                type="text"
+                // autoComplete="new-password"
+                fullWidth
+              />
+
+              <Field
+                key="password"
+                name="password"
+                label="密码"
+                // placeholder="保证不偷看"
+                className={classes.item}
+                component={TextField}
+                type="password"
+                autoComplete="new-password"
+                fullWidth
+              />
+
+              <Button
+                variant="contained"
+                size="large"
+                color="primary"
+                type="submit"
+                className={classes.button}
+                // disabled={!dirty && !valid}
+                fullWidth
+              >
+                {loading && <CircularProgress style={{ marginRight: 8 }} color="inherit" size={14} thickness={5} />}
+                登录
+              </Button>
+            </form>
+          );
+        }}
+      />
+    );
+  }
+}
--- view/login/login.js
+++ view/login/login.js
@@ -1,26 +1,11 @@
 import React, { PureComponent } from 'react';
-import { Form, Field } from 'react-final-form';
 import { withStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import TextField from '@/components/Form/TextField';
-import CircularProgress from '@material-ui/core/CircularProgress';
 import { Mutation } from 'react-apollo';
 import { USER_LOGIN } from '@/graphql/schema/user';
 import Snackbar from '@/components/Snackbar';
 import { setStorage } from '@/utils/store';
 import { USER_TOKEN } from '@/config/base';
-
-const registerValidate = (values) => {
-  const errors = {};
-  if (!values.username) {
-    errors.username = '用户名不能为空';
-  }
-  if (!values.password) {
-    errors.password = '密码不能为空';
-  }
-  console.log(errors);
-  return errors;
-};
+import PhoneLogin from './components/Form/Login';
 
 const styles = (theme) => {
   return {
@@ -32,65 +17,6 @@ const styles = (theme) => {
 
 @withStyles(styles)
 export default class LoginForm extends PureComponent {
-  renderForm =(onLogin, loading) => {
-    const { classes } = this.props;
-    return (
-      <Form
-        onSubmit={onLogin}
-        validate={registerValidate}
-        render={({ handleSubmit, values, valid, dirty, ...other }) => {
-          // console.log('other');
-          // console.log(other);
-          return (
-            <form id="createArticleForm" onSubmit={handleSubmit}>
-
-              <Field
-                key="username"
-                name="username"
-                label="用户名"
-                placeholder="请输入手机号或邮箱"
-                className={classes.item}
-                component={TextField}
-                type="text"
-                // autoComplete="new-password"
-                fullWidth
-              />
-
-              <Field
-                key="password"
-                name="password"
-                label="密码"
-                // placeholder="保证不偷看"
-                className={classes.item}
-                component={TextField}
-                type="password"
-                autoComplete="new-password"
-                fullWidth
-              />
-
-              <Button
-                variant="contained"
-                size="large"
-                color="primary"
-                type="submit"
-                style={{
-                  height: 48,
-                  boxShadow: '0 8px 30px rgba(0,0,0,0.1)',
-                  borderRadius: 6,
-                }}
-                // disabled={!dirty && !valid}
-                fullWidth
-              >
-                {loading && <CircularProgress style={{ marginRight: 8 }} color="inherit" size={14} thickness={5} />}
-                登录
-              </Button>
-            </form>
-          );
-        }}
-      />
-    );
-  }
-
   render() {
     const { onLoginSuccess } = this.props;
     return (
@@ -121,7 +47,7 @@ export default class LoginForm extends PureComponent {
               console.log(err);
             }
           };
-          return this.renderForm(onLogin, loading);
+          return <PhoneLogin onSubmit={onLogin} loading={loading} />;
         }}
       </Mutation>
     );
