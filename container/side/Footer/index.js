0f94e97f5d23790f7ac9f9745c246819c0dbf63d
liumin1128 <970568830@qq.com>
2018-12-07 12:57:13 +0800

    更新

--- container/side/Footer/index.js
+++ container/side/Footer/index.js
@@ -7,6 +7,19 @@ export default class index extends PureComponent {
     return (
       <div>
         <Typography style={{ color: '#999' }} variant="body2" gutterBottom>
+          <Link href="mailto:970568830@qq.com">联系我们</Link>
+          {' - '}
+          <a
+            onClick={() => {
+              alert('如有疑问，请联系QQ：97056883，或微信：qq970568830');
+            }}
+            style={{ textDecoration: 'none', color: '#999' }}
+            href="#"
+          >
+            投诉建议
+          </a>
+        </Typography>
+        <Typography style={{ color: '#999' }} variant="body2" gutterBottom>
           盗火©2018 -
           {' '}
           <Link target="_blank" href="http://www.miitbeian.gov.cn">吉ICP备15006191号-2</Link>
