9f4bd852767977e7bf63c031a30662174687ea99
liumin1128 <970568830@qq.com>
2019-02-16 15:08:41 +0800

    更新

--- pages/article/detail.js
+++ pages/article/detail.js
@@ -1,27 +1,17 @@
 import React, { PureComponent } from 'react';
 import { withRouter } from 'next/router';
+import BaseLayout from '@/components/Layout/Base';
 import Detail from '@/container/article/Detail';
-import Hidden from '@material-ui/core/Hidden';
-import Grid from '@material-ui/core/Grid';
-import Side from '@/container/side';
+
 @withRouter
 export default class index extends PureComponent {
   render() {
     const { router } = this.props;
     const { _id } = router.query;
     return (
-      <div>
-        <Grid container spacing={16}>
-          <Grid item xs={12} sm={12} md={8}>
-            <Detail _id={_id} />
-          </Grid>
-          <Grid item xs={12} sm={12} md={4}>
-            <Hidden implementation="css" only={[ 'sm', 'xs' ]}>
-              <Side />
-            </Hidden>
-          </Grid>
-        </Grid>
-      </div>
+      <BaseLayout>
+        <Detail _id={_id} />
+      </BaseLayout>
     );
   }
 }
