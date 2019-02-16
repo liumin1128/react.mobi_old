ab041f893d7a985eb1eb15244c4490494ab3ddcb
liumin1128 <970568830@qq.com>
2019-02-16 15:05:14 +0800

    更新布局

--- components/Layout/Base/index.js
+++ components/Layout/Base/index.js
@@ -1,24 +1,40 @@
-import React, { PureComponent } from 'react';
+import React, { PureComponent, Fragment } from 'react';
 import Hidden from '@material-ui/core/Hidden';
 import Grid from '@material-ui/core/Grid';
 import Side from '@/container/side';
+import { withStyles } from '@material-ui/core/styles';
+import Header from '../Header';
 
+const styles = theme => ({
+  root: theme.container,
+  header: {
+    marginBottom: 16,
+  },
+});
+
+// @pageLoading
+@withStyles(styles)
 export default class index extends PureComponent {
   render() {
-    const { children } = this.props;
+    const { children, classes } = this.props;
     return (
-      <div>
-        <Grid container spacing={16}>
-          <Grid item xs={12} sm={12} md={8}>
-            {children}
+      <Fragment>
+        <div className={classes.header}>
+          <Header />
+        </div>
+        <div className={classes.root}>
+          <Grid container spacing={16}>
+            <Grid item xs={12} sm={12} md={8}>
+              {children}
+            </Grid>
+            <Grid item xs={12} sm={12} md={4}>
+              <Hidden implementation="css" only={[ 'sm', 'xs' ]}>
+                <Side />
+              </Hidden>
+            </Grid>
           </Grid>
-          <Grid item xs={12} sm={12} md={4}>
-            <Hidden implementation="css" only={[ 'sm', 'xs' ]}>
-              <Side />
-            </Hidden>
-          </Grid>
-        </Grid>
-      </div>
+        </div>
+      </Fragment>
     );
   }
 }
--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,6 @@ import App, { Container } from 'next/app';
 import { ApolloProvider } from 'react-apollo';
 import hoc from '@/hoc';
 import { isServerSide } from '@/utils/common';
-import Layout from '@/components/Layout';
 import dynamicFile from '@/utils/dynamicFile';
 
 
@@ -12,7 +11,6 @@ export default class MyApp extends App {
   componentDidMount() {
     this.load();
   }
-
 
   load = async () => {
     await dynamicFile([
@@ -23,31 +21,14 @@ export default class MyApp extends App {
 
   render() {
     const { Component, pageProps, pageContext, apolloClient, router } = this.props;
-
-    if (router.pathname.indexOf('/login') !== -1) {
-      return (
-        <Container>
-          <ApolloProvider client={apolloClient}>
-            {
-              isServerSide()
-                ? <Component pageContext={pageContext} {...pageProps} />
-                : <Component {...pageProps} />
-            }
-          </ApolloProvider>
-        </Container>
-      );
-    }
-
     return (
       <Container>
         <ApolloProvider client={apolloClient}>
-          <Layout>
-            {
-              isServerSide()
-                ? <Component pageContext={pageContext} {...pageProps} />
-                : <Component {...pageProps} />
-            }
-          </Layout>
+          {
+            isServerSide()
+              ? <Component pageContext={pageContext} {...pageProps} />
+              : <Component {...pageProps} />
+          }
         </ApolloProvider>
       </Container>
     );
