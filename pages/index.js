a73962df9dd74b98be6f3588b978a65c0e0a9737
liumin1128 <970568830@qq.com>
2019-02-16 18:00:38 +0800

    更新

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,9 @@
-import React, { PureComponent } from 'react';
-import BaseLayout from '@/components/Layout/Base';
+
+import Layout from '@/components/Layout/Blank';
 import Article from '@/container/article/list';
 
-export default class index extends PureComponent {
-  render() {
-    return (
-      <BaseLayout>
-        <Article />
-      </BaseLayout>
-    );
-  }
-}
+export default () => (
+  <Layout>
+    <Article />
+  </Layout>
+);
