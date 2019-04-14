a57c34b107edaa90e8fba0e13ca504fa87c9aac1
liumin1128 <970568830@qq.com>
2019-04-14 09:57:26 +0800

    gx1

--- container/News/Detail/index.js
+++ container/News/Detail/index.js
@@ -5,15 +5,25 @@ import { NEWS_DETAIL } from '@/graphql/schema/news';
 import Loading from '@/components/Loading';
 import Html from '@/components/Html';
 import Card from '@material-ui/core/Card';
+import Avatar from '@material-ui/core/Avatar';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 import Link from '@/components/Link';
+import { formatTime, getTimeAgo } from '@/utils/common';
+
 // import UserInfo from './UserInfo';
 
 // https://imgs.react.mobi/Fr-KDqr9RX0S-uyQhHci7kgW4Bgk
 // https://imgs.react.mobi/FhYeU4wkGY4EwFUrpFQIt8ffhO7X
 // https://imgs.react.mobi/Fv7dReRsJkHrQhaESz_v2J1klUXX
+
+const appLogo = {
+  '3dmgame.com': 'https://imgs.react.mobi/FhyQn2l-CNmlB-Oxi9pXvm0vZqSJ',
+  'toutiao.com': 'https://imgs.react.mobi/Fk0vSrOldS7bHybywpj3pKXTMWEZ',
+  'eastday.com': 'https://imgs.react.mobi/FvCrL3RB16fjHafbjXhrrg3zOmGA',
+  'qq.com': 'https://imgs.react.mobi/FkIgsT1b8qL52F8fKidNimDhKUJB',
+};
 
 @withRouter
 export default class NewsDetail extends PureComponent {
@@ -26,14 +36,21 @@ export default class NewsDetail extends PureComponent {
           if (error) return 'error';
           if (loading) return <Loading />;
 
-          const { user, title, cover, html, url, content, showHtml, appCode, photos } = data;
+          const { user, title, cover, html, url, content, showHtml, appCode, appName, photos, createdAt } = data;
 
           return (
             <div style={{ maxWidth: 700 }}>
-              {appCode}
-              <br />
-              <a href={url} target="_blank">{url}</a>
               <Typography variant="h6">{title}</Typography>
+              <br />
+              <div style={{ display: 'flex', alignItems: 'center', fontSize: 12, color: '#666', opacity: 0.8 }}>
+                <a href={url} target="_blank" style={{ flex: 1, margin: 0, display: 'flex', alignItems: 'center', textDecoration: 'none' }}>
+                  <Avatar aria-label="Recipe" src={appLogo[appCode]} style={{ maxWidth: 18, maxHeight: 18, fontSize: 10, marginRight: 8 }}>
+                    {appName[0]}
+                  </Avatar>
+                  <p style={{ margin: 0, color: '#666' }}>{appName}</p>
+                </a>
+                <p style={{ margin: 0 }}>{getTimeAgo(createdAt)}</p>
+              </div>
               <br />
               <Html html={html} />
             </div>
