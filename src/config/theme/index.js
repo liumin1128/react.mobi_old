636faccac16f1f25f06a80b12224e9a2028b2ebd
liumin1128 <970568830@qq.com>
2019-06-06 23:30:16 +0800

    更新

--- src/config/theme/index.js
+++ src/config/theme/index.js
@@ -5,18 +5,18 @@ import Color from 'color';
 
 // Create a theme instance.
 const theme = createMuiTheme({
-  overrides: {
-    MuiAppBar: {
-      root: {
-        background: `linear-gradient(60deg, ${Color(cyan.A700).lighten(0.1)}, ${Color(cyan.A700).darken(0)})`,
-        boxShadow: `0 4px 8px 0px ${Color(cyan.A700).alpha(0.3)}, 0 3px 5px -2px ${Color(cyan.A700).alpha(0.5)}`,
-      },
-    },
-  },
+  // overrides: {
+  //   MuiAppBar: {
+  //     root: {
+  //       background: `linear-gradient(60deg, ${Color(cyan.A700).lighten(0.1)}, ${Color(cyan.A700).darken(0)})`,
+  //       boxShadow: `0 4px 8px 0px ${Color(cyan.A700).alpha(0.3)}, 0 3px 5px -2px ${Color(cyan.A700).alpha(0.5)}`,
+  //     },
+  //   },
+  // },
   palette: {
     primary: {
       ...cyan,
-      contrastText: '#fff'
+      contrastText: '#fff',
     },
     secondary: {
       main: '#19857b',
