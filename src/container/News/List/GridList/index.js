1d0247018b90b7807b67f5ff4f82c05f108a4e08
liumin1128 <970568830@qq.com>
2019-06-08 00:45:50 +0800

    更新

--- src/container/News/List/GridList/index.js
+++ src/container/News/List/GridList/index.js
@@ -1,4 +1,5 @@
 import React, { PureComponent, Fragment } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import { withRouter } from 'next/router';
 import { Query } from 'react-apollo';
 import { Waypoint } from 'react-waypoint';
@@ -14,105 +15,77 @@ import CardMedia from '@material-ui/core/CardMedia';
 import CardHeader from '@material-ui/core/CardHeader';
 import { NEWS_LIST } from '@/graphql/schema/news';
 import { getTimeAgo } from '@/utils/common';
-import Link from '@/components/Link'
-import Loading from '@/components/Loading'
+import Link from '@/components/Link';
+import Loading from '@/components/Loading';
 import { updateQuery } from '@/graphql/utils';
+import { useQuery } from 'react-apollo-hooks';
 
-const appLogo = {
-  '3dmgame.com': 'https://imgs.react.mobi/FhyQn2l-CNmlB-Oxi9pXvm0vZqSJ',
-  'toutiao.com': 'https://imgs.react.mobi/Fk0vSrOldS7bHybywpj3pKXTMWEZ',
-  'eastday.com': 'https://imgs.react.mobi/FvCrL3RB16fjHafbjXhrrg3zOmGA',
-  'qq.com': 'https://imgs.react.mobi/FkIgsT1b8qL52F8fKidNimDhKUJB',
-};
+const useStyles = makeStyles({
+  cover: {
+    paddingTop: '56%',
+    backgroundColor: 'rgba(0,0,0,0.05)',
+    marginBottom: 12,
+    borderRadius: 4,
+  },
+  title: {
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
+    display: '-webkit-box',
+    '-webkit-line-clamp': '1',
+    '-webkit-box-orient': 'vertical',
+    // minHeight: '3em',
+  },
+});
 
 
-@withRouter
-export default class NewsList extends PureComponent {
-  render() {
-    const { router } = this.props;
-    const { search, tag, type } = router.query;
+function NewsList({ router }) {
+  const classes = useStyles();
+
+  const { data, error, loading } = useQuery(NEWS_LIST);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
     return (
-        <Query
-          query={NEWS_LIST}
-          variables={{ search, tag, type }}
-        >
-          {({ loading, error, data = {}, fetchMore, refetch }) => {
-
-            if (loading) return <CircularProgress color="secondary" />
-
-            if (error) {
-              return (
-                <div>
-                  {`Error! ${error.message}`}
-                  <a onClick={() => { refetch({ search, tag }); }}>refetch</a>
-                </div>
-              );
-            }
-
-            const { list = [] } = data;
-
-            const loadMore = () => fetchMore({
-              variables: {
-                skip: list.length,
-                search,
-              },
-              updateQuery,
-            });
-
-            return (
-              <Fragment>
-
-                <Grid container spacing={2}>
-                  {list
-                    .map((i, index) => (
-                    <Grid key={i._id} item xs={12} md={4}>
-                      <Link href={"/news/detail?_id="+i._id}>
-                        <CardMedia
-                          style={{
-                            paddingTop: '56%',
-                            backgroundColor: 'rgba(0,0,0,0.05)',
-                            marginBottom: 12,
-                            borderRadius: 4
-                          }}
-                          image={i.cover || Array.isArray(i.photos) ? i.photos[0] : ''  }
-                        />
-                        <Typography 
-                          variant="subtitle2" 
-                          color="textPrimary" 
-                          style={{ overflow: 'hidden' }}
-                        >
-                          {i.title}
-                        </Typography>
-
-                      </Link>
-
-                      <div style={{display: 'flex', alignItems: 'center'}}>
-                        <Typography variant="caption" gutterBottom style={{marginRight: 8}}>
-                          {i.appName}
-                        </Typography>
-                        <Typography variant="caption" gutterBottom style={{flex: 1}}>
-                          {getTimeAgo(i.createdAt)}
-                        </Typography>
-                        
-                        <IconButton
-                          // size="small"
-                          color="inherit"
-                          aria-label="More"
-                          aria-haspopup="true"
-                        >
-                          <MoreHorizIcon fontSize="small" color="inherit" />
-                        </IconButton>
-                      </div>
-
-                    </Grid>
-                  ))}
-                </Grid>
-                <Loading />
-                <Waypoint onEnter={loadMore} />
-              </Fragment>
-            );
-          }}
-        </Query>
+      <div>
+        {error.message}
+      </div>
     );
   }
+
+
+  console.log('data');
+  console.log(data);
+
+  return (
+    <Fragment>
+      <Grid container spacing={3}>
+        {data.list.map((i, index) => (
+          <Grid key={i._id} item xs={12} md={4}>
+            <Link href={`/news/detail?_id=${i._id}`}>
+              <CardMedia className={classes.cover} image={i.cover || Array.isArray(i.photos) ? i.photos[0] : ''} />
+              <Typography variant="subtitle2" color="textPrimary" className={classes.title}>{i.title}</Typography>
+            </Link>
+
+            {
+
+            // <Box display="flex" alignItems="center">
+            //   <Typography variant="caption" gutterBottom style={{ marginRight: 8 }}>{i.appName}</Typography>
+            //   <Typography variant="caption" gutterBottom style={{ flex: 1 }}>{getTimeAgo(i.createdAt)}</Typography>
+              // <IconButton color="inherit" aria-label="More" aria-haspopup="true">
+              //   <MoreHorizIcon fontSize="small" color="inherit" />
+              // </IconButton>
+            // </Box>
+            }
+
+          </Grid>
+        ))}
+      </Grid>
+    </Fragment>
+  );
 }
+
+
+export default withRouter(NewsList);
