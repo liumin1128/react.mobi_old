6416f97db76edb480636e063ee21c0327021423b
liumin1128 <970568830@qq.com>
2019-06-08 12:22:14 +0800

    更新

--- pages/bxgif/list.js
+++ pages/bxgif/list.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Container from '@material-ui/core/Container';
+import Box from '@material-ui/core/Box';
 import Content from '@/container/bxgif/list';
 
 function Index() {
   return (
     <Container maxWidth="md">
-      <Content />
+      <Box my={2}>
+        <Content />
+      </Box>
     </Container>
   );
 }
