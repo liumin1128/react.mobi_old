60ef703ac5b27886d5148bdc5ec736f4109b4ace
liumin1128 <970568830@qq.com>
2019-06-08 11:50:12 +0800

    更新

--- src/container/News/List/GridList/styles.js
+++ src/container/News/List/GridList/styles.js
@@ -1,18 +1,43 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles(theme => ({
+  '@keyframes rotate': {
+    from: {
+      'background-position': '-300px',
+    },
+    to: {
+      'background-position': '0px',
+    },
+  },
+
   cover: {
+
     paddingTop: '56%',
     backgroundColor: 'rgba(128,128,128,0.3)',
     marginBottom: 12,
-    borderRadius: 4,
-    borderBottomRightRadius: 0,
-    transition: 'background-size 0.2s',
-    backgroundSize: '100%',
+    // borderRadius: 4,
+
+    position: 'relative',
+
     '&:hover': {
-      backgroundSize: '110%',
-      // border: '1px red solid',
+      '&::after': {
+        content: "''",
+        position: 'absolute',
+        width: 'calc(100% + 4px)',
+        height: 'calc(100% + 4px)',
+
+        // borderRadius: 4,
+
+        top: -2,
+        left: -2,
+        display: 'block',
+        zIndex: -1,
+
+        animation: '$rotate 5s infinite linear',
+        background: '-webkit-gradient(linear, left top, right top, color-stop(0%,rgba(48,255,144,1)), color-stop(25%,rgba(237,45,237,1)), color-stop(50%,rgba(201,152,38,1)), color-stop(75%,rgba(48,255,230,1)), color-stop(100%,rgba(48,255,144,1)))',
+      },
     },
+
 
   },
   title: {
@@ -27,3 +52,16 @@ export default makeStyles(theme => ({
     },
   },
 }));
+
+
+// .gradient {
+//   background: rgb(48,255,144); /* Old browsers */
+// background: -moz-linear-gradient(left,  rgba(48,255,144,1) 0%, rgba(237,45,237,1) 25%, rgba(201,152,38,1) 50%, rgba(48,255,230,1) 75%, rgba(48,255,144,1) 100%); /* FF3.6+ */
+// background: -webkit-gradient(linear, left top, right top, color-stop(0%,rgba(48,255,144,1)), color-stop(25%,rgba(237,45,237,1)), color-stop(50%,rgba(201,152,38,1)), color-stop(75%,rgba(48,255,230,1)), color-stop(100%,rgba(48,255,144,1))); /* Chrome,Safari4+ */
+// background: -webkit-linear-gradient(left,  rgba(48,255,144,1) 0%,rgba(237,45,237,1) 25%,rgba(201,152,38,1) 50%,rgba(48,255,230,1) 75%,rgba(48,255,144,1) 100%); /* Chrome10+,Safari5.1+ */
+// background: -o-linear-gradient(left,  rgba(48,255,144,1) 0%,rgba(237,45,237,1) 25%,rgba(201,152,38,1) 50%,rgba(48,255,230,1) 75%,rgba(48,255,144,1) 100%); /* Opera 11.10+ */
+// background: -ms-linear-gradient(left,  rgba(48,255,144,1) 0%,rgba(237,45,237,1) 25%,rgba(201,152,38,1) 50%,rgba(48,255,230,1) 75%,rgba(48,255,144,1) 100%); /* IE10+ */
+// background: linear-gradient(to right,  rgba(48,255,144,1) 0%,rgba(237,45,237,1) 25%,rgba(201,152,38,1) 50%,rgba(48,255,230,1) 75%,rgba(48,255,144,1) 100%); /* W3C */
+// filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#30ff90', endColorstr='#30ff90',GradientType=1 ); /* IE6-9 */
+
+// }
