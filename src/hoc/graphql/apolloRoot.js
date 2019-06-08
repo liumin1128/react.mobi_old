ee20178b8bfd4f67eab62a98ea69503737caf85f
liumin1128 <970568830@qq.com>
2019-06-08 11:55:14 +0800

    更新

--- src/hoc/graphql/apolloRoot.js
+++ src/hoc/graphql/apolloRoot.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { getDataFromTree, ApolloProvider } from 'react-apollo';
+import { getMarkupFromTree } from 'react-apollo-hooks';
 import Head from 'next/head';
-
+import { renderToString } from 'react-dom/server';
 import initApollo from './initApollo';
 
 export default (App) => {
@@ -39,14 +40,27 @@ export default (App) => {
         // and extract the resulting data
         try {
           // Run all GraphQL queries
-          await getDataFromTree(
-            <App
-              {...appProps}
-              Component={Component}
-              router={router}
-              apolloClient={apollo}
-            />,
-          );
+          // await getDataFromTree(
+          //   <App
+          //     {...appProps}
+          //     Component={Component}
+          //     router={router}
+          //     apolloClient={apollo}
+          //   />,
+          // );
+
+          // 使用hook形式的ssr渲染器
+          await getMarkupFromTree({
+            renderFunction: renderToString,
+            tree: (
+              <App
+                {...appProps}
+                Component={Component}
+                router={router}
+                apolloClient={apollo}
+              />
+            ),
+          });
         } catch (error) {
           console.error('Error while running `getDataFromTree`', error);
         }
