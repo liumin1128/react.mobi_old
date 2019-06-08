4e3388e87abe0b622d244d5d51861752b8ff42c9
liumin1128 <970568830@qq.com>
2019-06-08 12:04:51 +0800

    更新

--- pages/news/detail.js
+++ pages/news/detail.js
@@ -2,13 +2,16 @@ import React, { PureComponent } from 'react';
 import Container from '@material-ui/core/Container';
 import Header from '@/components/Layout/Header';
 import Content from '@/container/News/Detail';
+import Box from '@material-ui/core/Box';
 
 
 class Index extends PureComponent {
   render() {
     return (
       <Container maxWidth="md">
-        <Content />
+        <Box my={2}>
+          <Content />
+        </Box>
       </Container>
     );
   }
--- pages/news/index.js
+++ pages/news/index.js
@@ -1,10 +1,8 @@
-
 import React, { PureComponent } from 'react';
 import Container from '@material-ui/core/Container';
 import Box from '@material-ui/core/Box';
 import Header from '@/components/Layout/Header';
 import Content from '@/container/News/List';
-
 
 class Index extends PureComponent {
   render() {
