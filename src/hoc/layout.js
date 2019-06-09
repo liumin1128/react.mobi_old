45592d3fe411a5ceb6552d337cac8d90fd3fa90b
liumin1128 <970568830@qq.com>
2019-06-09 11:15:07 +0800

    更新

--- src/hoc/layout.js
+++ src/hoc/layout.js
@@ -1,4 +1,7 @@
 import React, { Fragment } from 'react';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import Hidden from '@material-ui/core/Hidden';
 import Header from '@/components/Layout/Header';
 
 export default WrappedComponent => ({ Component, ...props }) => {
@@ -6,7 +9,29 @@ export default WrappedComponent => ({ Component, ...props }) => {
   if (!Component.Layout) Component.Layout = Fragment;
   if (!Component.Header) Component.Header = Header;
   if (!Component.Footer) Component.Footer = Fragment;
-  if (!Component.Sider) Component.Sider = Fragment;
 
-  return <WrappedComponent Component={Component} {...props} />;
+  if (!Component.Sider) {
+    return (
+      <Container maxWidth="md">
+        <WrappedComponent Component={Component} {...props} />
+      </Container>
+    );
+  } else {
+    Component.Sider = Fragment;
+  }
+
+  return (
+    <Container maxWidth="md">
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={12} md={8}>
+          <WrappedComponent Component={Component} {...props} />
+        </Grid>
+        <Grid item xs={12} sm={12} md={4}>
+          <Hidden implementation="css" only={[ 'sm', 'xs' ]}>
+            <Component.Sider />
+          </Hidden>
+        </Grid>
+      </Grid>
+    </Container>
+  );
 };
