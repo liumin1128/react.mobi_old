ffc5fdd52bb436ff7d8194135388983860332666
liumin1128 <970568830@qq.com>
2019-06-09 20:25:42 +0800

    更新

--- src/container/News/Detail/index.js
+++ src/container/News/Detail/index.js
@@ -1,16 +1,17 @@
 import React, { PureComponent } from 'react';
-import { Query } from 'react-apollo';
+import { useQuery } from 'react-apollo-hooks';
 import { withRouter } from 'next/router';
 import { NEWS_DETAIL } from '@/graphql/schema/news';
 import Loading from '@/components/Loading';
 import Html from '@/components/Html';
-import Card from '@material-ui/core/Card';
+import Button from '@material-ui/core/Button';
 import Avatar from '@material-ui/core/Avatar';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 import Link from '@/components/Link';
 import { formatTime, getTimeAgo } from '@/utils/common';
+import useStyles from './styles';
 
 // import UserInfo from './UserInfo';
 
@@ -25,41 +26,44 @@ const appLogo = {
   'qq.com': 'https://imgs.react.mobi/FkIgsT1b8qL52F8fKidNimDhKUJB',
 };
 
-@withRouter
-export default class NewsDetail extends PureComponent {
-  render() {
-    const { router } = this.props;
-    const { _id } = router.query;
+function MzituDetail({ router }) {
+  const classes = useStyles();
+  const { _id } = router.query;
+  const { data, error, loading, refetch } = useQuery(NEWS_DETAIL, { variables: { _id } });
+
+  if (loading) return <Loading />;
+
+  if (error) {
     return (
-      <Query query={NEWS_DETAIL} variables={{ _id }}>
-        {({ loading, error, data: { data } }) => {
-          if (error) return 'error';
-          if (loading) return <Loading />;
-
-          const { user, title, cover, html, url, content, showHtml, appCode, appName, photos, createdAt } = data;
-
-          return (
-            <div>
-              <Typography variant="h4" style={{ fontWeight: 600 }}>{title}</Typography>
-              <br />
-              <div style={{ display: 'flex', alignItems: 'center', fontSize: 12, color: '#666', opacity: 0.8 }}>
-                <a href={url} target="_blank" style={{ margin: 0, display: 'flex', alignItems: 'center', textDecoration: 'none' }}>
-                  <Avatar aria-label="Recipe" src={appLogo[appCode]} style={{ maxWidth: 18, maxHeight: 18, fontSize: 10, marginRight: 8 }}>
-                    {appName[0]}
-                  </Avatar>
-                  <p style={{ margin: 0, color: '#666', marginRight: 8 }}>
-                    {appName}
-                    {'  '}
-                  </p>
-                </a>
-                <p style={{ margin: 0 }}>{formatTime(createdAt, 'YYYY-MM-DD HH:mm:ss')}</p>
-              </div>
-              <br />
-              <Html html={html} />
-            </div>
-          );
-        }}
-      </Query>
+      <div>
+        {`Error! ${error.message}`}
+        <Button onClick={() => { refetch({ _id: id }); }}>refetch</Button>
+      </div>
     );
   }
+
+  const { data: { user, title, cover, html, url, content, showHtml, appCode, appName, photos, createdAt } } = data;
+
+  return (
+    <div>
+      <Typography variant="h4" style={{ fontWeight: 600 }}>{title}</Typography>
+      <br />
+      <div style={{ display: 'flex', alignItems: 'center', fontSize: 12, color: '#666', opacity: 0.8 }}>
+        <a href={url} target="_blank" style={{ margin: 0, display: 'flex', alignItems: 'center', textDecoration: 'none' }}>
+          <Avatar aria-label="Recipe" src={appLogo[appCode]} style={{ maxWidth: 18, maxHeight: 18, fontSize: 10, marginRight: 8 }}>
+            {appName[0]}
+          </Avatar>
+          <p style={{ margin: 0, color: '#666', marginRight: 8 }}>
+            {appName}
+            {'  '}
+          </p>
+        </a>
+        <p style={{ margin: 0 }}>{formatTime(createdAt, 'YYYY-MM-DD HH:mm:ss')}</p>
+      </div>
+      <br />
+      <Html html={html} />
+    </div>
+  );
 }
+
+export default withRouter(MzituDetail);
