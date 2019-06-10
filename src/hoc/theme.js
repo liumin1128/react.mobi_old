95a03d727c24bd69465029e020392655386cffe8
liumin1128 <970568830@qq.com>
2019-06-10 11:22:43 +0800

    更新

--- src/hoc/theme.js
+++ src/hoc/theme.js
@@ -1,4 +1,4 @@
-import React, { PureComponent, createContext } from 'react';
+import React, { PureComponent, createContext, useReducer, useContext } from 'react';
 import defaultTheme from '@/config/theme';
 import darkTheme from '@/config/theme/dark';
 
@@ -9,8 +9,32 @@ const themes = {
 
 export const ThemeContext = createContext(defaultTheme);
 
+// ThemeProvider的hoc用法
+export function withThemeProvider(WrappedComponent) {
+  return class ThemeContextProvider extends PureComponent {
+      state = {
+        theme: 'dark',
+      }
 
-// 一个方便操作的hoc，直接传进theme
+      setTheme = () => {
+        const { theme } = this.state;
+        this.setState({
+          theme: theme === 'default' ? 'dark' : 'default',
+        });
+      }
+
+      render() {
+        const { theme } = this.state;
+        return (
+          <ThemeContext.Provider value={{ theme: themes[theme], setTheme: this.setTheme }}>
+            <WrappedComponent {...this.props} />
+          </ThemeContext.Provider>
+        );
+      }
+  };
+}
+
+// theme的hoc用法
 export function withThemeConsumer(WrappedComponent) {
   return props => (
     <ThemeContext.Consumer>
@@ -19,28 +43,25 @@ export function withThemeConsumer(WrappedComponent) {
   );
 }
 
-export function withThemeProvider(WrappedComponent) {
-  return class ThemeContextProvider extends PureComponent {
-      state = {
-        theme: 'dark',
-      }
+// ThemeProvider的hook用法
+export default function ThemeProvider({ children }) {
+  function reducer(state, action) {
+    switch (action.type) {
+      case 'switch':
+        return { theme: state.theme === 'default' ? 'dark' : 'default' };
+      default:
+        return state;
+    }
+  }
+  const [ state, dispatch ] = useReducer(reducer, { count: 0 });
+  return (
+    <ThemeContext.Provider value={{ state, dispatch }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+}
 
-      render() {
-        const { theme } = this.state;
-        return (
-          <ThemeContext.Provider
-            value={{
-              theme: themes[theme],
-              setTheme: () => {
-                this.setState({
-                  theme: theme === 'default' ? 'dark' : 'default',
-                });
-              },
-            }}
-          >
-            <WrappedComponent {...this.props} />
-          </ThemeContext.Provider>
-        );
-      }
-  };
+// theme的hook用法
+export function useTheme() {
+  return useContext(ThemeContext);
 }
