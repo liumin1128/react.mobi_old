bfcbc140f5368529a47e63f0b0f71c50f0d7226f
liumin1128 <970568830@qq.com>
2019-06-11 14:37:23 +0800

    更新

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,22 @@
 
-import React from 'react';
+import React, { Fragment } from 'react';
 import GridList from '@/container/News/List/GridList';
+import NavTabs from '@/components/NavTabs';
 // import FlatList from '@/container/News/List/FlatList';
 
 function Index() {
   return (
-    <GridList />
+    <Fragment>
+      <NavTabs
+        navList={[
+          { label: 'switch', pathname: '/' },
+          { label: 'ps4', pathname: '/ps4' },
+          { label: 'cosplay', pathname: '/cos' },
+        ]}
+      />
+      <br />
+      <GridList />
+    </Fragment>
   );
 }
 
