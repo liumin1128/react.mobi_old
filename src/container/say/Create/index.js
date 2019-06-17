95d868677b3238ed0133449ba6d83808a869cb09
liumin1128 <970568830@qq.com>
2019-06-17 18:58:08 +0800

    更新

--- src/container/say/Create/index.js
+++ src/container/say/Create/index.js
@@ -8,7 +8,7 @@ import CardMedia from '@material-ui/core/CardMedia';
 
 import Link from '@/components/Link';
 import Loading from '@/components/Loading';
-import { SAY_CREATE } from '@/graphql/schema/say';
+import { SAY_CREATE, SAY_LIST } from '@/graphql/schema/say';
 import { useMutation } from '@/hooks/graphql';
 import useStyles from './styles';
 
@@ -32,3 +32,29 @@ function SayCreate({ router }) {
 }
 
 export default withRouter(SayCreate);
+
+
+// 乐观更新
+// optimisticResponse: { result: { status: 200, message: '创建成功11', __typename: 'Result' } },
+// update: (store, { data: { result: { status } } }) => {
+//   console.log('status');
+//   console.log(status);
+//   if (status === 200) {
+//     // Read the data from our cache for this query.
+//     const data = store.readQuery({ query: SAY_LIST });
+//     // Add our comment from the mutation to the end.
+//     data.list.push({
+//       content: '测试文本22',
+//       createdAt: '1560768742714',
+//       user: {
+//         nickname: '本王今年八岁',
+//         avatarUrl: 'https://imgs.react.mobi/Fs0w36NQ9R1szSwKO3c0nIor_8m6',
+//         __typename: 'User',
+//       },
+//       __typename: 'Say',
+//       _id: '5d0770e6bd48e7125d2c5971',
+//     });
+//     // Write our data back to the cache.
+//     store.writeQuery({ query: SAY_LIST, data });
+//   }
+// },
