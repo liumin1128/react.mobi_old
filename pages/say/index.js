b6f7e3624d26f4088e55d67708a896d7c67af1ce
liumin1128 <970568830@qq.com>
2019-06-18 20:21:36 +0800

    /say

--- pages/say/index.js
+++ pages/say/index.js
@@ -1,6 +1,6 @@
 import React, { PureComponent } from 'react';
 import BaseLayout from '@/components/Layout/Base';
-import List from '@/container/say/list';
+import List from '@/container/Say/List';
 
 export default class index extends PureComponent {
   render() {
