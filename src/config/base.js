59def769343f601b5f4298ec0976e072dbbafe77
liumin1128 <970568830@qq.com>
2019-06-18 19:59:39 +0800

    更新

--- src/config/base.js
+++ src/config/base.js
@@ -4,7 +4,7 @@ export const LOGO_URL = 'https://imgs.react.mobi/FldU5XAVJksEDNDEs7MZiF36DMAz';
 
 export const NAV_TABS = [
   { pathname: '/', label: '资讯' },
-  { pathname: '/shequ', label: '社区' },
+  // { pathname: '/shequ', label: '社区' },
   { pathname: '/bxgif/list', label: '囧图' },
   { pathname: '/mzitu', label: '福利' },
   // { pathname: '/about', label: '关于' },
