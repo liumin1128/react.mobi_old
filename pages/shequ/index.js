1db7ec0cb0f52db45d48de070540a4c8b47404f2
liumin1128 <970568830@qq.com>
2019-07-11 14:49:18 +0800

    更新

--- pages/shequ/index.js
+++ pages/shequ/index.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import Card from '@material-ui/core/Card';
+import Box from '@material-ui/core/Box';
 import SayList from '@/container/Say/List';
 import SayCreate from '@/container/Say/Create';
+import NavTabs from '@/components/NavTabs';
 
 function Index() {
   return (
-    <SayList />
+    <Fragment>
+      <Card>
+        <NavTabs
+          navList={[
+            { label: 'switch', pathname: '/' },
+            { label: 'ps4', pathname: '/ps4' },
+            { label: 'cosplay', pathname: '/cos' },
+          ]}
+        />
+      </Card>
+      <Box pb={2} />
+      <SayList />
+    </Fragment>
   );
 }
 
