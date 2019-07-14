9250d61b0cc90e730ee0846cf680b2b6a187f1b2
liumin1128 <970568830@qq.com>
2019-07-14 17:27:53 +0800

    更新

--- src/container/Dynamic/List/Item/index.js
+++ src/container/Dynamic/List/Item/index.js
@@ -6,6 +6,7 @@ import CardHeader from '@material-ui/core/CardHeader';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
 import Avatar from '@material-ui/core/Avatar';
+import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
@@ -13,6 +14,8 @@ import ThumbUpIcon from '@material-ui/icons/ThumbUpOutlined';
 import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
 import Comment from '@/container/Comment';
 import { getTimeAgo } from '@/utils/common';
+import CreateComment from '@/container/Comment/Create';
+import CommentList from '@/container/Comment/List';
 import useStyles from './styles';
 import { text2html } from '../../utils';
 
@@ -30,45 +33,47 @@ function DynamicListItem({ _id, content, pictures = [], topics, user, createdAt,
   return (
     <Fragment key={_id}>
       <Card className={classes.card}>
-        <CardHeader
-          avatar={(<Avatar aria-label="Avatar" src={user.avatarUrl} className={classes.avatar}>{user.nickname}</Avatar>)}
-          action={(
-            <IconButton aria-label="Settings">
-              <MoreVertIcon />
-            </IconButton>
-            )}
-          title={<Typography variant="h6" className={classes.nickname}>{user.nickname}</Typography>}
-          subheader={getTimeAgo(createdAt)}
-        />
-        <Box ml={10}>
-          <CardContent className={classes.content}>
+        <Box p={3} pb={0}>
+          <CardHeader
+            className={classes.header}
+            avatar={(<Avatar aria-label="Avatar" src={user.avatarUrl} className={classes.avatar}>{user.nickname}</Avatar>)}
+            action={(<IconButton aria-label="Settings"><MoreVertIcon /></IconButton>)}
+            title={<Typography variant="h6" className={classes.nickname}>{user.nickname}</Typography>}
+            subheader={getTimeAgo(createdAt)}
+
+          />
+          <Box ml={8} mt={1}>
             <Typography variant="body1" gutterBottom component="div">
               <div className={classes.html} dangerouslySetInnerHTML={{ __html: html }} />
             </Typography>
-            <Box display="flex" m={-0.5} mt={1}>
+
+            {pictures.length > 0 && (
+            <Box display="flex" m={-0.5} mt={1} mb={1}>
               {pictures.map(i => <CardMedia key={i} className={classes.picture} image={i} />)}
             </Box>
-          </CardContent>
+            )}
 
-          <Box pb={2} display="flex">
-            <Box style={{ display: 'flex', alignItems: 'center', color: '#999' }}>
-              <ChatBubbleOutlineIcon style={{ width: 16, marginTop: 2, marginRight: 8 }} />
+            <Box mb={2} display="flex">
+              <Box style={{ display: 'flex', alignItems: 'center', color: '#999' }}>
+                <ChatBubbleOutlineIcon style={{ width: 16, marginTop: 2, marginRight: 8 }} />
               11
-            </Box>
-            <Box mr={5} />
-            <Box style={{ display: 'flex', alignItems: 'center', color: '#999' }}>
-              <ThumbUpIcon style={{ width: 16, marginTop: 2, marginRight: 8 }} />
-              <img src="" alt="" />
+              </Box>
+              <Box mr={5} />
+              <Box style={{ display: 'flex', alignItems: 'center', color: '#999' }}>
+                <ThumbUpIcon style={{ width: 16, marginTop: 2, marginRight: 8 }} />
+                <img src="" alt="" />
               11
+              </Box>
             </Box>
           </Box>
 
-
+          <Box>
+            <Divider />
+            <CreateComment session={_id} />
+            <CommentList session={_id} />
+          </Box>
         </Box>
 
-        <Box>
-          <Comment _id={_id} />
-        </Box>
 
       </Card>
       <Box mb={1.5} />
--- src/container/Dynamic/List/Item/styles.js
+++ src/container/Dynamic/List/Item/styles.js
@@ -1,8 +1,8 @@
 import { makeStyles, fade } from '@material-ui/core/styles';
 
 export default makeStyles(theme => ({
-  card: {
-    padding: theme.spacing(1),
+  header: {
+    padding: 0,
   },
   avatar: {
     width: 48,
