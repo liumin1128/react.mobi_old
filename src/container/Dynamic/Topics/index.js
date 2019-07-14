28fe644476fe280b4fc29063e72fc598093297aa
liumin1128 <970568830@qq.com>
2019-07-14 20:29:02 +0800

    更新

--- src/container/Dynamic/Topics/index.js
+++ src/container/Dynamic/Topics/index.js
@@ -4,6 +4,7 @@ import Typography from '@material-ui/core/Typography';
 import Card from '@material-ui/core/Card';
 import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
+import Link from '@/components/Link';
 import { useMutation } from '@/hooks/graphql';
 import { DYNAMIC_TOPIC_LIST } from '@/graphql/schema/dynamic';
 import { useOnMount } from '@/hooks';
@@ -26,9 +27,12 @@ function Topics({ onClick }) {
         <Typography variant="subtitle2" gutterBottom>热门话题</Typography>
         <Box mx={-1.2}>
           {topics.length > 0 && topics.map(i => (
-            <Button key={i._id} size="small" color="primary" button onClick={() => onClick(i)}>
-              {`#${i.title}#`}
-            </Button>
+            <Link key={i._id} href={`/dynamic?topic=${i.number}`}>
+              <Button size="small" color="primary">
+                {`#${i.title}#`}
+              </Button>
+            </Link>
+
           ))}
         </Box>
       </Box>
