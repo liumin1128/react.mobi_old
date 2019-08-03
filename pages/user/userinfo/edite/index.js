b73cc8b06bfd3e5bd5114ce49e05ab09f01665c8
liumin1128 <970568830@qq.com>
2019-08-03 17:40:11 +0800

    更新

---
+++ pages/user/userinfo/edite/index.js
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+import { Form, Field } from 'react-final-form';
+import TextField from '@/components/Form/TextField';
+import { useMutation } from '@/hooks/graphql';
+import { USERINFO } from '@/graphql/schema/user';
+import { useOnMount } from '@/hooks';
+
+function EditeUserInfo() {
+  const [ getUserInfo, { called, data, error, hasError, loading } ] = useMutation(USERINFO);
+  useOnMount(async () => {
+    await getUserInfo();
+  });
+
+  if (!called || loading) return 'loading';
+  if (hasError) return error;
+
+  console.log('data');
+  console.log(data);
+
+  const { userInfo } = data;
+
+  console.log(userInfo);
+
+
+  return (
+    <div>
+      <Form
+        onSubmit={(values) => {
+          console.log('values');
+          console.log(values);
+        }}
+        initialValues={userInfo}
+        render={({ handleSubmit }) => (
+          <form onSubmit={handleSubmit}>
+            <Field
+              fullWidth
+              margin="normal"
+              name="avatarUrl"
+              label="头像"
+              component={TextField}
+            />
+            <Field
+              fullWidth
+              margin="normal"
+              name="nickname"
+              label="用户昵称"
+              component={TextField}
+            />
+            <Field
+              fullWidth
+              margin="normal"
+              name="phone"
+              label="手机号"
+              type="tel"
+              component={TextField}
+            />
+          </form>
+        )}
+      />
+    </div>
+  );
+}
+
+export default EditeUserInfo;
--- src/components/Form/TextField.js
+++ src/components/Form/TextField.js
@@ -2,22 +2,16 @@ import React from 'react';
 import TextField from '@material-ui/core/TextField';
 
 export default ({
-  input: { name, onChange, value, ...restInput },
+  input,
   meta,
   ...rest
 }) => {
-  // 加了restInput，会导致样式异常，无法focus
-  // console.log('restInput');
-  // console.log(restInput);
   return (
     <TextField
+      {...input}
       {...rest}
-      name={name}
       helperText={meta.touched ? meta.error : undefined}
       error={meta.error && meta.touched}
-      // inputProps={restInput}
-      onChange={onChange}
-      value={value}
     />
   );
 };
