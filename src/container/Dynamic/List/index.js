1ed12687de396d2f0187b32bba0897c477c0578d
liumin1128 <970568830@qq.com>
2019-08-07 19:36:32 +0800

    更新

--- src/container/Dynamic/List/index.js
+++ src/container/Dynamic/List/index.js
@@ -1,5 +1,7 @@
 import React, { Fragment, useState } from 'react';
 import { withRouter } from 'next/router';
+import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
 import { DYNAMIC_LIST, CHECK_NEW_DYNAMIC } from '@/graphql/schema/dynamic';
 import { useQuery, useMutation } from '@/hooks/graphql';
 import useLoop from '@/hooks/loop';
@@ -9,7 +11,7 @@ import Item from './Item';
 
 function DynamicList({ router }) {
   const [ hasNewData, setNewData ] = useState(false);
-  const { data, error, loading, isLoadingMore, isEnd, loadMore } = useQuery(DYNAMIC_LIST, router.query);
+  const { data, error, loading, isLoadingMore, isEnd, loadMore, refetch } = useQuery(DYNAMIC_LIST, router.query);
 
   const [ check ] = useMutation(CHECK_NEW_DYNAMIC);
 
@@ -19,7 +21,9 @@ function DynamicList({ router }) {
     await check({ latest: list[0].createdAt }, {
       update: (_, { data: { result: { status: code, message } } }) => {
         console.log(code, message);
-        setNewData(true);
+        if (code === 200) {
+          setNewData(true);
+        }
       },
     });
   }
@@ -33,6 +37,22 @@ function DynamicList({ router }) {
 
   return (
     <Fragment>
+
+      {hasNewData && (
+        <Button
+          color="primary"
+          variant="outlined"
+          fullWidth
+          onClick={() => {
+            setNewData(false);
+            refetch();
+          }}
+        >
+          有新动态，点击查看
+        </Button>
+      )}
+
+      <Box mt={1.5} />
 
       {list.map(i => <Item key={i._id} {...i} />)}
 
