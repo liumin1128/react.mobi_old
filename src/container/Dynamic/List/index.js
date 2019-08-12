8be9b5c5b6d368ab10a376d7d376733f4fa66ffb
liumin1128 <970568830@qq.com>
2019-08-12 15:00:22 +0800

    更新

--- src/container/Dynamic/List/index.js
+++ src/container/Dynamic/List/index.js
@@ -1,38 +1,65 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import { withRouter } from 'next/router';
 import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
 import { DYNAMIC_LIST, CHECK_NEW_DYNAMIC } from '@/graphql/schema/dynamic';
 import { useQuery, useMutation } from '@/hooks/graphql';
-import useLoop from '@/hooks/loop';
+// import { useOnMount, useOnUnmount } from '@/hooks';
+// import useLoop from '@/hooks/loop';
 import Loading from '@/components/Loading';
 import Loadmore from '@/components/Loading/Loadmore';
 import Item from './Item';
 
 function DynamicList({ router }) {
-  const [ hasNewData, setNewData ] = useState(false);
+  const [ hasNewData, setNewData ] = useState(true);
   const { data, error, loading, isLoadingMore, isEnd, loadMore, refetch } = useQuery(DYNAMIC_LIST, router.query);
 
   const [ check ] = useMutation(CHECK_NEW_DYNAMIC);
 
-  useLoop(checkNewDynamic, 10000);
-
-  async function checkNewDynamic() {
-    await check({ latest: list[0].createdAt }, {
-      update: (_, { data: { result: { status: code, message } } }) => {
-        console.log(code, message);
-        if (code === 200) {
-          setNewData(true);
-        }
-      },
-    });
-  }
 
   if (loading) return <Loading />;
 
   if (error) return <div>{error.message}</div>;
 
   const { list } = data;
+
+  // function checkNewDynamic(latest) {
+  //   return async function () {
+  //     console.log(list[0].content, new Date(parseInt(latest, 0)));
+  //     // alert(list[0].content, new Date(parseInt(list[0].createdAt, 0)));
+  //     await check({ latest }, {
+  //       update: (_, { data: { result: { status: code, message } } }) => {
+  //         console.log(code, message);
+  //         if (code === 200) {
+  //           setNewData(true);
+  //         }
+  //       },
+  //     });
+  //   };
+  // }
+
+  // const [ loop ] = useLoop();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      console.log(list[0].content);
+
+      check({ latest: list[0].createdAt }, {
+        update: (_, { data: { result: { status: code, message } } }) => {
+          console.log(code, message);
+          if (code === 200) {
+            setNewData(true);
+            clearTimeout(timer);
+          }
+        },
+      });
+    }, 5000);
+
+
+    return function cleanup() {
+      clearTimeout(timer);
+    };
+  });
 
 
   return (
@@ -44,6 +71,8 @@ function DynamicList({ router }) {
           variant="outlined"
           fullWidth
           onClick={() => {
+            // alert(list[0].content, new Date(parseInt(list[0].createdAt, 0)));
+            // loop(checkNewDynamic(list[0].createdAt), 10000);
             setNewData(false);
             refetch();
           }}
--- src/hooks/loop.js
+++ src/hooks/loop.js
@@ -2,19 +2,22 @@ import { useOnMount, useOnUnmount } from '@/hooks';
 
 let timer;
 
-export default function useLoop(cb, t = 60000) {
-  useOnMount(() => {
-    loop();
-  });
+export default function useLoop() {
+  // useOnMount(() => {
+  //   loop();
+  // });
 
   useOnUnmount(() => {
     clearTimeout(timer);
   });
 
-  function loop() {
+  function loop(cb, t = 60000) {
+    clearTimeout(timer);
     timer = setTimeout(() => {
       cb();
-      loop();
+      loop(cb, t);
     }, t);
   }
+
+  return [ loop ];
 }
