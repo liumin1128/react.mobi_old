6c2b10edb5a3210070f102e0df8ae5cd2e5afbdf
liumin1128 <970568830@qq.com>
2019-08-28 17:07:42 +0800

    懒加载iframe

--- src/container/Dynamic/components/Iframe/index.js
+++ src/container/Dynamic/components/Iframe/index.js
@@ -1,11 +1,21 @@
 
 import React from 'react';
+import LazyLoad from 'react-lazyload';
+
 import useStyles from './styles';
 
 function Video({ iframe }) {
   const classes = useStyles();
   return (
-    <div className={classes.iframe} dangerouslySetInnerHTML={{ __html: iframe }} />
+
+    <LazyLoad
+      debounce={100}
+      unmountIfInvisible
+      placeholder={<div className={classes.iframe} />}
+    >
+      <div className={classes.iframe} dangerouslySetInnerHTML={{ __html: iframe }} />
+
+    </LazyLoad>
   );
 }
 
