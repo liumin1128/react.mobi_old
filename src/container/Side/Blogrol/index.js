abbbe0d4b4326949c1c9007d057bc41cf3935de3
liumin1128 <970568830@qq.com>
2019-09-24 08:25:15 +0800

    修复link

--- src/container/Side/Blogrol/index.js
+++ src/container/Side/Blogrol/index.js
@@ -1,21 +1,35 @@
 import React, { PureComponent } from 'react';
 import Typography from '@material-ui/core/Typography';
-import Link from '@/components/Link';
+import useStyles from './styles';
 
-export default class index extends PureComponent {
-  render() {
-    return (
-      <div>
-        <Typography variant="subtitle2" gutterBottom>
+export default function () {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <Typography variant="subtitle2" gutterBottom>
           友情链接
-        </Typography>
-        <Typography variant="body2" gutterBottom>
-          <Link target="_blank" href="https://www.xiaoduyu.com">小度鱼 - xiaoduyu.com</Link>
-        </Typography>
-        <Typography variant="body2" gutterBottom>
-          <Link target="_blank" href="https://liumin.me">八岁的博客 - liumin.me</Link>
-        </Typography>
-      </div>
-    );
-  }
+      </Typography>
+      <Typography variant="body2" gutterBottom>
+        <a
+          rel="noopener noreferrer"
+          target="_blank"
+          href="https://www.xiaoduyu.com"
+          className={classes.a}
+        >
+            小度鱼 - xiaoduyu.com
+        </a>
+      </Typography>
+      <Typography variant="body2" gutterBottom>
+        <a
+          rel="noopener noreferrer"
+          target="_blank"
+          href="https://liumin.me"
+          className={classes.a}
+        >
+            八岁的博客 - liumin.me
+        </a>
+      </Typography>
+    </div>
+  );
 }
--- src/container/Side/Footer/index.js
+++ src/container/Side/Footer/index.js
@@ -1,30 +1,31 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import Link from '@/components/Link';
+import useStyles from './styles';
 
-export default class index extends PureComponent {
-  render() {
-    return (
-      <div>
-        <Typography style={{ color: '#999' }} variant="body2" gutterBottom>
-          <Link href="mailto:970568830@qq.com">联系我们</Link>
-          {' - '}
-          <a
-            onClick={() => {
-              alert('如有疑问，请联系QQ：97056883，或微信：qq970568830');
-            }}
-            style={{ textDecoration: 'none', color: '#999' }}
-            href="#"
-          >
+export default function () {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <Typography style={{ color: '#999' }} variant="body2" gutterBottom>
+        <a className={classes.a} href="mailto:970568830@qq.com">联系我们</a>
+        {' - '}
+        <a
+          onClick={() => {
+            alert('如有疑问，请联系QQ：97056883，或微信：qq970568830');
+          }}
+          style={{ textDecoration: 'none', color: '#999' }}
+          href="#"
+        >
             投诉建议
-          </a>
-        </Typography>
-        <Typography style={{ color: '#999' }} variant="body2" gutterBottom>
+        </a>
+      </Typography>
+      <Typography style={{ color: '#999' }} variant="body2" gutterBottom>
           盗火 -
-          {' '}
-          <Link target="_blank" href="http://www.miitbeian.gov.cn">吉ICP备15006191号-2</Link>
-        </Typography>
-      </div>
-    );
-  }
+        {' '}
+        <Link target="_blank" href="http://www.miitbeian.gov.cn">吉ICP备15006191号-2</Link>
+      </Typography>
+    </div>
+  );
 }
