f1d0ce76e04eb3906b5771c3a802777e64f2bf33
liumin1128 <970568830@qq.com>
2019-09-24 10:06:50 +0800

    控制返回顶部按钮

--- src/components/BackToTop/index.js
+++ src/components/BackToTop/index.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import throttle from 'lodash/throttle';
 import Box from '@material-ui/core/Box';
 import useStyles from './styles';
-import { scrollToTop } from '@/utils/common';
+import { scrollToTop, getScrollTop } from '@/utils/common';
+import { useOnMount } from '@/hooks';
+
 
 export default function () {
   const classes = useStyles();
+
+  const [ visible, setVisible ] = useState(false);
+
+  function updateVisible() {
+    return throttle(() => {
+      const scrollTop = getScrollTop();
+      if (scrollTop > 300) {
+        setVisible(true);
+      } else {
+        setVisible(false);
+      }
+    }, 300, { leading: true })();
+  }
+
+
+  useEffect(() => {
+    window.addEventListener('scroll', updateVisible);
+    return function cleanup() {
+      window.removeEventListener('scroll', updateVisible);
+    };
+  });
+
+  if (!visible) return null;
 
   return (
     <Box className={classes.root} onClick={scrollToTop}>
