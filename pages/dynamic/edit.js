55ab916814c5cb94acf27a2a7cacbb34dcbe6e08
liumin1128 <970568830@qq.com>
2019-09-26 09:07:55 +0800

    更新

--- pages/dynamic/edit.js
+++ pages/dynamic/edit.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import Card from '@material-ui/core/Card';
+import Box from '@material-ui/core/Box';
+
 import Content from '@/container/Dynamic/Edit';
 
 function DynamicDetail() {
   return (
-    <Content />
+    <Box display="flex" justifyContent="center">
+      <Box width="100%" maxWidth={600}>
+        <Card>
+          <Box p={2}>
+            <Content />
+          </Box>
+        </Card>
+      </Box>
+    </Box>
+
   );
 }
 
