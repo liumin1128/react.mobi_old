bebc320b7e64dde41a8ecb5084968014b54bea45
liumin1128 <970568830@qq.com>
2019-09-28 03:30:13 +0800

    更新

--- src/container/User/ProfileCard/index.js
+++ src/container/User/ProfileCard/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import Link from 'next/link';
+
 import Box from '@material-ui/core/Box';
 import Card from '@material-ui/core/Card';
 import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
 import { useMutation } from '@/hooks/graphql';
@@ -18,7 +21,18 @@ function EditeUserInfo() {
   });
 
   if (!getUserInfoData.called || getUserInfoData.loading) return 'loging';
-  if (getUserInfoData.hasError) return getUserInfoData.error;
+  if (getUserInfoData.hasError) {
+    return (
+      <Card>
+        <Box p={2} display="flex" justifyContent="center">
+          <Link href="/login/register"><Button color="primary" variant="outlined">注册</Button></Link>
+          <Box ml={1} />
+          <Link href="/login"><Button color="primary" variant="contained">登录</Button></Link>
+        </Box>
+      </Card>
+
+    );
+  }
 
   const { userInfo } = getUserInfoData.data;
 
