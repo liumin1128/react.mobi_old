5c2d96ba35d38deb9b23d56b0bda0f8584b652f4
liumin1128 <970568830@qq.com>
2019-10-08 14:57:11 +0800

    感谢

--- src/container/Side/Code/index.js
+++ src/container/Side/Code/index.js
@@ -1,19 +1,20 @@
 import React, { PureComponent } from 'react';
-import Card from '@material-ui/core/Card';
-import Button from '@material-ui/core/Button';
-import CardContent from '@material-ui/core/CardContent';
 import { withStyles } from '@material-ui/core/styles';
 import CardMedia from '@material-ui/core/CardMedia';
 import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
-import Link from '@/components/Link';
 
-const styles = theme => ({
+const styles = (theme) => ({
   media: {
     height: 0,
     paddingTop: '100%',
     marginBottom: 8,
-    opacity: 0.8,
+    opacity: 0.6,
+  },
+  media2: {
+    height: 0,
+    paddingTop: '100%',
+    marginBottom: 8,
+    margin: -2,
   },
 });
 
@@ -32,6 +33,16 @@ export default class index extends PureComponent {
             title="Contemplative Reptile"
           />
         </Grid>
+
+        <Grid item xs={6}>
+          <CardMedia
+            className={classes.media2}
+            image="https://imgs.react.mobi/FlL5SMO_2SIfBkWPWJERNd79GN4D"
+            title="Contemplative Reptile"
+          />
+        </Grid>
+
+
         {
           //   <Grid item xs={12} md={6}>
           //   <CardMedia
