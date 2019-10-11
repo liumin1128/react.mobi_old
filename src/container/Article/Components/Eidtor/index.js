88b1d28b1117bb916ec5bbded9c064f50967247e
liumin1128 <970568830@qq.com>
2019-10-11 09:06:14 +0800

    新建基础编辑器

--- src/container/Article/Components/Eidtor/index.js
+++ src/container/Article/Components/Eidtor/index.js
@@ -1,15 +1,54 @@
+import React, { useState, useRef, useEffect } from 'react';
+// import ReactDOM from 'react-dom';
+import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
+import { Editor, EditorState } from 'draft-js';
+import { state2json, state2html } from './utils';
 
-import React from 'react';
-// import FlatList from '@/container/News/List/FlatList';
 
-function Index() {
+function MyEditor() {
+  const [ editorState, setEditorState ] = useState(
+    EditorState.createEmpty(),
+  );
+
+  const editor = useRef();
+
+  function focusEditor() {
+    editor.current.focus();
+  }
+
+  useEffect(() => {
+    focusEditor();
+  }, []);
+
+  function onChange(state) {
+    setEditorState(state);
+  }
+
+
   return (
-    <>
-      Editor
-    </>
+    <Box onClick={focusEditor} bgcolor="#ddd" p={4}>
+      <Editor
+        ref={editor}
+        editorState={editorState}
+        onChange={onChange}
+      />
+      <Button
+        onClick={() => {
+          console.log(state2json(editorState));
+        }}
+      >
+        state2json
+      </Button>
+      <Button
+        onClick={() => {
+          console.log(state2html(editorState));
+        }}
+      >
+        state2html
+      </Button>
+    </Box>
   );
 }
 
-// Index.Sider = FlatList;
-
-export default Index;
+export default MyEditor;
--- src/container/Article/Create/index.js
+++ src/container/Article/Create/index.js
@@ -1,11 +1,17 @@
 
 import React from 'react';
-import Eidtor from '@/container/Article/Components/Eidtor';
+// import Eidtor from '@/container/Article/Components/Eidtor';
+import dynamic from 'next/dynamic';
+
+const EidtorWithNoSSR = dynamic(
+  () => import('@/container/Article/Components/Eidtor'),
+  { ssr: false, loading: () => 'loading' },
+);
 
 function Index() {
   return (
     <>
-      <Eidtor />
+      <EidtorWithNoSSR />
     </>
   );
 }
