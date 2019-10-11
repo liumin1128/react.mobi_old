6a6b2cc999a0acd6aaf0837b070a78f8d4da8e8b
liumin1128 <970568830@qq.com>
2019-10-12 01:04:16 +0800

    移除老旧代码

--- src/container/Article/Create/index.js
+++ src/container/Article/Create/index.js
@@ -1,6 +1,5 @@
 
 import React from 'react';
-// import Eidtor from '@/container/Article/Components/Eidtor';
 import dynamic from 'next/dynamic';
 import Box from '@material-ui/core/Box';
 import Paper from '@material-ui/core/Paper';
