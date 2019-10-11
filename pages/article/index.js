ed9fcb3588a632f462cbfab9cc9a0ef96cebadf2
liumin1128 <970568830@qq.com>
2019-10-11 08:33:59 +0800

    更新

---
+++ pages/article/index.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+// import FlatList from '@/container/News/List/FlatList';
+
+function Index() {
+  return (
+    <>
+      文章列表
+    </>
+  );
+}
+
+// Index.Sider = FlatList;
+
+export default Index;
