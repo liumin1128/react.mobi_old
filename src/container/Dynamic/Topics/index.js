753ac74a2c31bd358c7d5adb564200f0ac1f6b13
liumin1128 <970568830@qq.com>
2019-10-24 17:03:33 +0800

    动态话题

--- src/container/Dynamic/Topics/index.js
+++ src/container/Dynamic/Topics/index.js
@@ -1,30 +1,36 @@
 
-import React, { useState } from 'react';
+import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import Card from '@material-ui/core/Card';
 import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
+import { useQuery } from '@apollo/react-hooks';
 import Link from '@/components/Link';
-import { useMutation } from '@/hooks/graphql';
 import { DYNAMIC_TOPIC_LIST } from '@/graphql/schema/dynamic';
-import { useOnMount } from '@/hooks';
+import Skeleton from './Skeleton';
 
 function Topics() {
-  const [ topics, setTopics ] = useState([]);
-  const [ getTopics ] = useMutation(DYNAMIC_TOPIC_LIST, {}, {
-    update: (_, { data }) => {
-      setTopics(data.list);
-    },
-  });
-  useOnMount(() => {
-    getTopics();
-  });
+  const { data, loading } = useQuery(DYNAMIC_TOPIC_LIST);
+
+  if (loading) {
+    return (
+      <Card>
+        <Box p={2}>
+          <Typography variant="subtitle2" gutterBottom>热门话题</Typography>
+          <Box mt={2} />
+          <Skeleton />
+        </Box>
+      </Card>
+    );
+  }
+  const { list } = data;
+
   return (
     <Card>
       <Box p={2}>
         <Typography variant="subtitle2" gutterBottom>热门话题</Typography>
         <Box mx={-1.2}>
-          {topics.length > 0 && topics.map(i => (
+          {list.length > 0 && list.map((i) => (
             <Link key={i._id} href={`/dynamic?topic=${i.number}`}>
               <Button size="small" color="primary">
                 {`#${i.title}#`}
