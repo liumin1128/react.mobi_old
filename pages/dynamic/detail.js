139ec42830e89c899930a5bfa1ec1c3c4b5b7750
liumin1128 <970568830@qq.com>
2019-11-08 14:31:12 +0800

    更新详情

--- pages/dynamic/detail.js
+++ pages/dynamic/detail.js
@@ -2,11 +2,18 @@ import React from 'react';
 import withApollo from '@/lib/apollo';
 import withLayout from '@/hoc/layout';
 import Content from '@/container/Dynamic/Detail';
+// import ProfileCard from '@/container/User/ProfileCard';
 
 function Index() {
   return (
     <Content />
   );
 }
+
+// Index.Sider = () => (
+//   <>
+//     <ProfileCard />
+//   </>
+// );
 
 export default withApollo(withLayout(Index));
--- src/container/User/ProfileCard/index.js
+++ src/container/User/ProfileCard/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { useQuery } from '@/hooks/graphql';
-import { USERINFO } from '@/graphql/schema/user';
+import { USERINFO, USERINFO_BY_ID } from '@/graphql/schema/user';
 import Skeleton from './Skeleton';
 import ProfileCard from './ProfileCard';
 import Sign from './Sign';
 
-function EditeUserInfo() {
-  const { data, loading, error } = useQuery(USERINFO, {}, { ssr: false });
+function EditeUserInfo({ _id }) {
+  const { data, loading, error } = useQuery(
+    _id ? USERINFO_BY_ID : USERINFO,
+    _id ? { _id } : {},
+    { ssr: false },
+  );
 
   if (loading) return <Skeleton />;
 
