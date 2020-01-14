550bc221df5c7f78fc18adc9bcd51412d7ded5e7
liumin1128 <970568830@qq.com>
2020-01-14 22:45:11 +0800

    gx1

--- pages/test/xlsx.js
+++ pages/test/xlsx.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, {PureComponent} from 'react';
 import Typography from '@material-ui/core/Typography';
-import Card from '@material-ui/core/Card';
+// import Card from '@material-ui/core/Card';
 import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
+import CircularProgress from '@material-ui/core/CircularProgress';
+
 // import vCardsJS from "vcards-js";
-import XLSX from "xlsx";
+// import XLSX from "xlsx";
+import dynamicFile from 'dynamic-file';
 
 export const xlsx2Json = (file) => {
   return new Promise((resolve, reject) => {
@@ -12,15 +15,14 @@ export const xlsx2Json = (file) => {
     reader.readAsBinaryString(file);
     reader.onload = (e) => {
       const data = e.target.result;
-      const temp = XLSX.read(data, {
+      const temp = window.XLSX.read(data, {
         type: 'binary',
       });
-      const result = XLSX.utils.sheet_to_json(temp.Sheets[temp.SheetNames[0]]);
+      const result = window.XLSX.utils.sheet_to_json(temp.Sheets[temp.SheetNames[0]]);
       resolve(result);
     };
   });
 };
-
 
 export const json2vcf = (data, name) => {
 
@@ -56,26 +58,57 @@ END:VCARD
   return string;
 }
 
-function Index() {
-  async function handleChange(e) {
+class Index extends PureComponent {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      status: 'loading'
+    }
+  }
+
+  async componentDidMount() {
+
+    await dynamicFile([
+      '/xlsx.full.min.js',
+    ]);
+
+    this.setState({ status: 'done'})
+
+
+  }
+
+  handleChange = async (e) => {
     const { files } = e.target;
     const name = files[0].name.split('.')[0];
     const list = await xlsx2Json(files[0]);
     json2vcf(getVcfData(list), name);
   }
-  return (
+
+  render() {
+    const { status } = this.state;
+
+    if(status === 'loading') return (
     <Box p={4} display="flex" flexDirection="column" justifyContent="center" alignItems="center">
+    
+    <CircularProgress />
+
+    </Box>)
+
+    return (
+      <Box p={4} display="flex" flexDirection="column" justifyContent="center" alignItems="center">
       <Typography variant="h6">Excel转通讯录</Typography>
       <Box mb={3}></Box>
 
       <Button variant="contained" color="primary" mt={2} style={{ width:'100%', maxWidth: '300px', textAlign:'center', padding: 16,position: 'relative', fontWeight: 'bold', borderRadius: 32 }}>
-        <input style={{ position: 'absolute', left: 0, top: 0, height: '100%',opacity: 0, width: '100%',backgroundColor: 'red' }} onChange={handleChange} type="file" />
-       点击转换
+        <input style={{ position: 'absolute', left: 0, top: 0, height: '100%',opacity: 0, width: '100%',backgroundColor: 'red' }} onChange={this.handleChange} type="file" />
+      点击转换
       </Button>
       <Box mb={3}></Box>
       <a href={'/tamplate.xlsx'} style={{ color: '#999'}}>点击下载模板</a>
     </Box>
-  );
+  )
+  }
 }
 
 export default Index;
