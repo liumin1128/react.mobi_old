e480b5fd07ece446808fef59cc8580717307d7fa
liumin1128 <970568830@qq.com>
2020-03-21 17:22:35 +0800

    更新

---
+++ pages/about/progress.js
@@ -0,0 +1,142 @@
+import React from 'react'
+import Card from '@material-ui/core/Card'
+import Box from '@material-ui/core/Box'
+import marked from 'marked'
+import Comment from '@/container/Comment'
+// import Html from '@/components/Html';
+import Blogrol from '@/container/Side/Blogrol'
+import Project from '@/container/Side/Project'
+import Code from '@/container/Side/Code'
+import Footer from '@/container/Side/Footer'
+import withApollo from '@/hoc/apollo'
+import withLayout from '@/hoc/layout'
+
+// marked.setOptions({
+//   highlight: function(code, lang, callback) {
+//     require('pygmentize-bundled') ({ lang: lang, format: 'html' }, code, function (err, result) {
+//       callback(err, result.toString());
+//     });
+//   }
+// });
+
+const md2 = `
+
+# 更新日志&开发计划&进度
+
+## 动态
+
+### 发布动态
+
+✅ 插入图片
+
+✅ 插入话题
+
+插入链接
+
+代码片段
+
+✅视频
+
+发起投票
+
+✅动态编辑
+
+✅动态删除
+
+
+### 用户简易富文本输入框
+
+✅颜文字
+
+✅Emoji
+
+✅字数统计与限制
+
+### 动态详情
+
+✅基础样式
+
+
+## 用户
+
+
+### 用户中心
+
+用户信息
+
+个人简历
+
+用户关注
+
+✅用户资料编辑
+
+用户第三方信息管理
+
+### 其他
+
+动态页用户信息
+
+连接动态
+
+✅ 开发进度展示
+
+✅ 开源集合
+
+
+***
+
+以后更新动态会发布到这里，有疑问请留意
+
+
+`
+
+function Index() {
+  return (
+    <div>
+      <Card>
+        <img
+          src='https://imgs.react.mobi/FpkanCXBRCWL5fMicEoGVgoJ6_TM'
+          style={{ width: '100%' }}
+          alt=''
+        />
+        <Box p={4}>
+          <div dangerouslySetInnerHTML={{ __html: marked(md2) }} />
+        </Box>
+      </Card>
+
+      <Box mt={2} />
+
+      <Comment _id='/pages/progress' />
+    </div>
+  )
+}
+
+Index.Sider = () => (
+  <>
+    <Card>
+      <Box p={2}>
+        <Blogrol />
+      </Box>
+    </Card>
+    <Box mt={2} />
+    <Card>
+      <Box p={2}>
+        <Project />
+      </Box>
+    </Card>
+    <Box mt={2} />
+    <Card>
+      <Box p={2}>
+        <Code />
+      </Box>
+    </Card>
+    <Box mt={2} />
+    <Card>
+      <Box p={2}>
+        <Footer />
+      </Box>
+    </Card>
+  </>
+)
+
+export default withApollo(withLayout(Index))
--- src/config/base.js
+++ src/config/base.js
@@ -1,25 +1,25 @@
-export const LOGO_URL = "https://imgs.react.mobi/FldU5XAVJksEDNDEs7MZiF36DMAz";
+export const LOGO_URL = 'https://imgs.react.mobi/FldU5XAVJksEDNDEs7MZiF36DMAz'
 // export const LOGO_URL = 'https://imgs.react.mobi/FjpmLe9it1pTemLeB2w5XgDhv1D-';
 // export const LOGO_URL = 'https://imgs.react.mobi/Fi6oyHGOpFKKAjNJilB5LSeTRurZ';
 
 export const NAV_TABS = [
-  { pathname: "/", label: "动态" },
+  { pathname: '/', label: '动态' },
   // { pathname: '/article', label: '文章' },
-  { pathname: "/news", label: "资讯" },
+  // { pathname: "/news", label: "资讯" },
   // { pathname: "/bxgif/list", label: "囧图" },
-  { pathname: "/about/download", label: "下载" }
+  { pathname: '/about/download', label: '下载' },
   // { pathname: '/mzitu', label: '福利' },
   // { pathname: '/about', label: '关于' },
-];
+]
 
-export const ENV = process.env.NODE_ENV !== "production";
+export const ENV = process.env.NODE_ENV !== 'production'
 
-export const STORE_USER_KEY = "react.mobi.user";
-export const USER_TOKEN = "react.mobi.user.token";
-export const USER_INFO_KEY = "react.mobi.user.info";
-export const USER_SETTING_THEME = "react.mobi.user.setting.theme";
-export const PATH_BEFORELOGIN = "react.mobi.path.before.login";
+export const STORE_USER_KEY = 'react.mobi.user'
+export const USER_TOKEN = 'react.mobi.user.token'
+export const USER_INFO_KEY = 'react.mobi.user.info'
+export const USER_SETTING_THEME = 'react.mobi.user.setting.theme'
+export const PATH_BEFORELOGIN = 'react.mobi.path.before.login'
 
-export const QINIUURL = "https://imgs.react.mobi";
-export const QINIU_UPLOADURL = "https://upload-z1.qiniup.com";
-export const API_URL = ENV ? "http://localhost:3101" : "https://api.react.mobi";
+export const QINIUURL = 'https://imgs.react.mobi'
+export const QINIU_UPLOADURL = 'https://upload-z1.qiniup.com'
+export const API_URL = ENV ? 'http://localhost:3101' : 'https://api.react.mobi'
