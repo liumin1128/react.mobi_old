837e34afc4b4be6ca18544dce91b6752da365ae3
liumin1128 <970568830@qq.com>
2020-03-21 13:35:31 +0800

    更新

--- src/components/Layout/Header/index.js
+++ src/components/Layout/Header/index.js
@@ -1,45 +1,45 @@
-import React from "react";
-import dynamic from "next/dynamic";
-import { makeStyles } from "@material-ui/core/styles";
-import Paper from "@material-ui/core/Paper";
-import Box from "@material-ui/core/Box";
-import Toolbar from "@material-ui/core/Toolbar";
-import AppBar from "@material-ui/core/AppBar";
-import ButtonBase from "@material-ui/core/ButtonBase";
-import Avatar from "@material-ui/core/Avatar";
-import IconButton from "@material-ui/core/IconButton";
-import ColorLensIcon from "@material-ui/icons/ColorLens";
-import ViewModuleIcon from "@material-ui/icons/ViewModule";
-import Hidden from "@material-ui/core/Hidden";
-import Link from "@/components/Link";
-import Popper from "@/components/Popper";
-import { LOGO_URL, NAV_TABS } from "@/config/base";
-import { useTheme } from "@/hoc/theme";
-import useElevationScroll from "@/hooks/useElevationScroll";
-import NavTabs from "./NavTabs";
+import React from 'react'
+import dynamic from 'next/dynamic'
+import { makeStyles } from '@material-ui/core/styles'
+import Paper from '@material-ui/core/Paper'
+import Box from '@material-ui/core/Box'
+import Toolbar from '@material-ui/core/Toolbar'
+import AppBar from '@material-ui/core/AppBar'
+import ButtonBase from '@material-ui/core/ButtonBase'
+import Avatar from '@material-ui/core/Avatar'
+import IconButton from '@material-ui/core/IconButton'
+import ColorLensIcon from '@material-ui/icons/ColorLens'
+import ViewModuleIcon from '@material-ui/icons/ViewModule'
+import Hidden from '@material-ui/core/Hidden'
+import Link from '@/components/Link'
+import Popper from '@/components/Popper'
+import { LOGO_URL, NAV_TABS } from '@/config/base'
+import { useTheme } from '@/hoc/theme'
+import useElevationScroll from '@/hooks/useElevationScroll'
+import NavTabs from './NavTabs'
 
-const DynamicUserInfoWithNoSSR = dynamic(() => import("./user"), {
-  ssr: false,
-  loading: () => <span />
-});
+// const DynamicUserInfoWithNoSSR = dynamic(() => import("./user"), {
+//   ssr: false,
+//   loading: () => <span />
+// });
 
 const useStyles = makeStyles(theme => ({
   root: {
-    height: 64
+    height: 64,
     //   [theme.breakpoints.down('xs')]: {
     //     height: 56,
     //   },
   },
   logo: {
-    marginRight: 16
+    marginRight: 16,
     // filter: 'grayscale(100%) saturate(100%)',
-  }
-}));
+  },
+}))
 
 function Header(props) {
-  const classes = useStyles();
-  const elevation = useElevationScroll({ elevation: 2 });
-  const { setTheme } = useTheme();
+  const classes = useStyles()
+  const elevation = useElevationScroll({ elevation: 2 })
+  const { setTheme } = useTheme()
 
   return (
     <div className={classes.root}>
@@ -51,7 +51,7 @@ function Header(props) {
             </ButtonBase>
           </Link> */}
           <Box flexGrow={1} className={classes.grow}>
-            <Hidden implementation="css" smUp>
+            <Hidden implementation='css' smUp>
               <Popper
                 content={
                   <Paper>
@@ -61,23 +61,23 @@ function Header(props) {
                   </Paper>
                 }
               >
-                <IconButton color="inherit">
+                <IconButton color='inherit'>
                   <ViewModuleIcon />
                 </IconButton>
               </Popper>
             </Hidden>
-            <Hidden implementation="css" smDown>
+            <Hidden implementation='css' smDown>
               <NavTabs navList={NAV_TABS} />
             </Hidden>
           </Box>
-          <IconButton color="inherit" onClick={setTheme}>
+          <IconButton color='inherit' onClick={setTheme}>
             <ColorLensIcon />
           </IconButton>
-          <DynamicUserInfoWithNoSSR />
+          {/* <DynamicUserInfoWithNoSSR /> */}
         </Toolbar>
       </AppBar>
     </div>
-  );
+  )
 }
 
-export default Header;
+export default Header
