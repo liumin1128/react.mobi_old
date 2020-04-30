c0b98698202fd1e1d5d818f646e601b88634d368
liumin1128 <970568830@qq.com>
2020-05-01 04:09:00 +0800

    gx1

--- pages/_error.js
+++ pages/_error.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Placeholder from '@/components/PlaceholderFigure'
 
-function Error({ statusCode }) {
+function Error() {
   return (
     <>
       <Placeholder.Error />
