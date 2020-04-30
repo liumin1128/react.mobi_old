1082a67bc0698861451f2eee65de7a23ee7b2a0a
liumin1128 <970568830@qq.com>
2020-05-01 04:06:58 +0800

    gx1

--- pages/_error.js
+++ pages/_error.js
@@ -1,21 +1,12 @@
-import React from "react";
-// import withLayout from "@/hoc/layout";
-import Placeholder from "@/components/PlaceholderFigure";
+import React from 'react'
+import Placeholder from '@/components/PlaceholderFigure'
 
 function Error({ statusCode }) {
-  if (statusCode === 404) {
-    return <Placeholder.Error404 />;
-  }
   return (
     <>
       <Placeholder.Error />
     </>
-  );
+  )
 }
 
-Error.getInitialProps = async ({ res, err }) => {
-  const statusCode = res ? res.statusCode : err ? err.statusCode : undefined;
-  return { statusCode };
-};
-
-export default Error;
+export default Error
