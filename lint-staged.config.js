d49072f9df21a279697cc74aa69a7d80acd63cbc
liumin1128 <970568830@qq.com>
2020-05-01 06:01:23 +0800

    fix: gx1

--- lint-staged.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  linters: {
-    '*.{ts,tsx}': ['eslint --fix', 'git add'],
-    '*.{js,jsx}': ['eslint --fix', 'git add'],
-    '*.{md,html,json}': ['prettier --write', 'git add'],
-    '*.{css,scss,less}': ['prettier --write', 'git add'],
-  },
-};
