f531efb7c7bd196fa84099ad71af4eeefb6a3398
liumin1128 <970568830@qq.com>
2020-05-23 08:30:02 +0800

    fix: randerDom

--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -3,10 +3,13 @@ import Box from '@material-ui/core/Box'
 import Toolbar from '@material-ui/core/Toolbar'
 import AppBar from '@material-ui/core/AppBar'
 import IconButton from '@material-ui/core/IconButton'
+import Button from '@material-ui/core/Button'
 import ColorLensIcon from '@material-ui/icons/ColorLens'
 import { makeStyles } from '@material-ui/core/styles'
 import { useTheme } from '@/hoc/theme'
 import useElevationScroll from '@/hooks/useElevationScroll'
+// import modal from '@/hoc/modal'
+import { domRender } from '@/utils/react'
 
 const useStyles = makeStyles(() => ({
   root: {
@@ -22,11 +25,18 @@ function Header() {
   const elevation = useElevationScroll({ elevation: 2 })
   const { setTheme } = useTheme()
 
+  function handleLogin() {
+    domRender(() => <>1111</>)
+  }
+
   return (
     <div className={classes.root}>
       <AppBar elevation={elevation} className={classes.root}>
         <Toolbar>
           <Box flexGrow={1}>Header</Box>
+          <Button onClick={handleLogin} color='inherit'>
+            Login
+          </Button>
           <IconButton color='inherit' onClick={setTheme}>
             <ColorLensIcon />
           </IconButton>
---
+++ src/utils/react.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+export const domRender = (Component: React.ComponentType<{ destory: () => void }>) => {
+  const element = document.createElement('div')
+  document.body.appendChild(element)
+  function destory() {
+    const unmountResult = ReactDOM.findDOMNode(element) // eslint-disable-line
+    if (unmountResult && element.parentNode) {
+      element.parentNode.removeChild(element)
+    }
+  }
+  ReactDOM.render(<Component destory={() => destory()} />, element)
+}
