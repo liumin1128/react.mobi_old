549ce422fb3fbf33dd61d2e3db5b42cde9165747
liumin1128 <970568830@qq.com>
2020-10-28 17:00:12 +0800

    gx1

---
+++ src/utils/test.tsx
@@ -0,0 +1,35 @@
+import { extend } from 'lodash'
+import React from 'react'
+
+// type HOC<T, U> = <Props>(
+//     Component: React.ComponentType<Props & InjectProps>
+//   ) => React.ComponentType<T & U>
+
+export function hoc<T extends Record<string, unknown>, U>(
+  Component: React.ComponentType<T>,
+  injectProps: U
+): (props: T) => React.ReactElement<T & U> {
+  return function (props: T): React.ReactElement<T & U> {
+    return <Component {...props} {...injectProps} />
+  }
+}
+class Test extends React.Component {
+    render() {
+    return <div>111</div>
+    }
+}
+interface SSProps {
+  color: string
+}
+
+const SS: React.ComponentType<SSProps> = ({ color }: SSProps) => <div>{color}</div>
+
+interface InjectProps {
+  size: string
+  height: number
+}
+
+const SSWithColor = hoc<SSProps, InjectProps>(SS, {})
+const SSWithColor2 = hoc<SSProps, InjectProps>(Test, { color: 'red' })
+
+console.log(<SSWithColor color='red' />)
